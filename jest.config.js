070f8c847fe481877688a348867e91f563c2c225
EvgeniiyaR <tulpansik@mail.ru>
2023-07-29 19:50:55 +0300

    fix: change moduleNameMapper

--- jest.config.js
+++ jest.config.js
@@ -1,8 +1,12 @@
+const { pathsToModuleNameMapper } = require('ts-jest');
+const { compilerOptions } = require('./tsconfig.json');
+
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'jsdom',
   moduleNameMapper: {
-    '^@src/(.*)$': '<rootDir>/src/$1',
-    '\\.(css|less)$': 'identity-obj-proxy',
+    ...pathsToModuleNameMapper(compilerOptions.paths, { prefix: '<rootDir>/' }),
+    '\\.(scss)$': 'identity-obj-proxy',
+    '\\.(jpg|jpeg|png|gif|svg)$': '<rootDir>/empty-module.js',
   },
 };
