1d70eb0f4b8d1873a0d85173e5338ec2fe4ea3c4
EvgeniiyaR <tulpansik@mail.ru>
2023-07-30 14:47:01 +0300

    fix: delete test data

--- src/components/Header/ExampleHeader.tsx
+++ src/components/Header/ExampleHeader.tsx
@@ -1,14 +1,10 @@
 import React from 'react';
 import './ExampleHeader.scss';
-import img from '@images/jpg/test.jpg';
 
 const ExampleHeader = (): React.ReactElement => {
   return (
     <>
-      <div className="test-back">
-        <h1 className="test">Hello, world</h1>
-      </div>
-      <img src={img} alt="test" />
+      <h1>Hello, world</h1>
     </>
   );
 };
