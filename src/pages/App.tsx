81d0f9224bfe38cb1abd763df8dd37357b20454c
Dimkov Alexandr <dimkovaleks4@gmail.com>
2023-08-02 18:51:09 +0300

    refactor: move App files to pages folder

---
+++ src/pages/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import './App.scss';
+import ExampleHeader from '@/components/Header/ExampleHeader';
+
+export default function App(): React.JSX.Element {
+  return (
+    <React.StrictMode>
+      <ExampleHeader />
+    </React.StrictMode>
+  );
+}
