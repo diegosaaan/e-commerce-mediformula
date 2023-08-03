cf45a21685bb9765f5a957153da1d6d5a11ada91
Dimkov Alexandr <dimkovaleks4@gmail.com>
2023-08-03 05:25:43 +0300

    refactor: rename routes creaters

--- src/routes/routesCreaters/privateRoutesCreater.tsx
+++ src/routes/routesCreaters/privateRoutesCreater.tsx
@@ -2,8 +2,8 @@ import React, { ReactElement } from 'react';
 import { Route, Navigate } from 'react-router-dom';
 import { IRouteData } from '@/types/interfaces';
 
-function privateRoutersCreater({ element, ...rest }: IRouteData, isAuthenticated: boolean): ReactElement {
+function privateRoutesCreater({ element, ...rest }: IRouteData, isAuthenticated: boolean): ReactElement {
   return <Route {...rest} element={isAuthenticated ? element : <Navigate to="/login" />} />;
 }
 
-export default privateRoutersCreater;
+export default privateRoutesCreater;
