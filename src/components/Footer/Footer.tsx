076ec5b084511d3442187a7df1323b1228d7265c
Dimkov Alexandr <dimkovaleks4@gmail.com>
2023-08-03 17:00:27 +0300

    feat: add Footer

---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,12 @@
+import React, { ReactElement } from 'react';
+import '@/components/Footer/Footer.scss';
+
+const Footer = (): ReactElement => {
+  return (
+    <footer>
+      <h1>Я футер</h1>
+    </footer>
+  );
+};
+
+export default Footer;
