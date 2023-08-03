cbbe4e4b5daddeca4b51f6f5bc6961bd656e0902
Dimkov Alexandr <dimkovaleks4@gmail.com>
2023-08-03 17:09:09 +0300

    feat: update App.tsx

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -1,11 +1,12 @@
 import './App.scss';
 import React, { ReactElement } from 'react';
+import { BrowserRouter } from 'react-router-dom';
 import AppRouter from '@/routes/AppRouter';
 
 export default function App(): ReactElement {
   return (
-    <React.StrictMode>
+    <BrowserRouter>
       <AppRouter />
-    </React.StrictMode>
+    </BrowserRouter>
   );
 }
