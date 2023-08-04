0b35d97380c15d1749991ed78f4c47900e1bf3ca
JuliaHoladava <juliaholadava@gmail.com>
2023-08-04 22:31:49 +0200

    feat: add button style

--- src/types/interfaces.ts
+++ src/types/interfaces.ts
@@ -1,4 +1,4 @@
-import { ReactElement } from 'react';
+import { ReactElement, ReactNode } from 'react';
 
 export interface IRouteData {
   key: string;
@@ -10,4 +10,12 @@ export interface IAuthContextValue {
   isUserLoggedIn: boolean;
   signIn: (cb: () => void) => void;
   signOut: (cb: () => void) => void;
+}
+
+export interface IPropsButton {
+  type: 'button' | 'submit' | 'reset';
+  text?: string;
+  children?: ReactNode;
+  className: string;
+  onClick: () => void;
 }
