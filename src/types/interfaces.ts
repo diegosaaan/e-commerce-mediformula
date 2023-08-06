73b07f71d319cc7779f9e5d99069fc621491f32c
Dimkov Alexandr <dimkovaleks4@gmail.com>
2023-08-06 08:42:14 +0300

    feat: add IAccordionProps interface

--- src/types/interfaces.ts
+++ src/types/interfaces.ts
@@ -1,4 +1,4 @@
-import { ReactElement } from 'react';
+import React, { ReactElement } from 'react';
 
 export interface IRouteData {
   key: string;
@@ -10,4 +10,13 @@ export interface IAuthContextValue {
   isUserLoggedIn: boolean;
   signIn: (cb: () => void) => void;
   signOut: (cb: () => void) => void;
+}
+
+export interface IAccordionProps {
+  sectionName: string;
+  listName: string;
+  title: string;
+  children: ReactElement;
+  isOpen: boolean;
+  onToogleAccordion: (event: React.KeyboardEvent | React.MouseEvent) => void;
 }
