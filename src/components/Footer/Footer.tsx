0760ca3345eaea40917c4c9a9241f39d8c1e3737
Dimkov Alexandr <dimkovaleks4@gmail.com>
2023-08-09 01:28:06 +0300

    feat: add links data and update Footer component

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -3,16 +3,13 @@ import '@/components/Footer/Footer.scss';
 import { Link } from 'react-router-dom';
 import YandexMap from './YandexMap';
 import Accordion from '../Accordion/Accrodion';
+import { accordionListData, socialIconsListData } from './linksData';
 
 const Footer = (): ReactElement => {
   const [activeAccordion, setActiveAccordion] = useState('');
 
-  const handleToogleAccordion = (event: React.KeyboardEvent | React.MouseEvent, listName: string): void => {
-    if ('key' in event.nativeEvent && event.nativeEvent.key === 'Enter') {
-      setActiveAccordion((prevActiveAccordion) => (prevActiveAccordion === listName ? '' : listName));
-    } else if ('type' in event.nativeEvent && event.nativeEvent.type === 'click') {
-      setActiveAccordion((prevActiveAccordion) => (prevActiveAccordion === listName ? '' : listName));
-    }
+  const handleToogleAccordion = (listName: string): void => {
+    setActiveAccordion((prevActiveAccordion) => (prevActiveAccordion === listName ? '' : listName));
   };
 
   return (
@@ -36,119 +33,34 @@ const Footer = (): ReactElement => {
             </ul>
 
             <div className="footer__accordion-container">
-              <Accordion
-                sectionName="footer"
-                listName="delivery"
-                title="Доставка и оплата"
-                isOpen={activeAccordion === 'delivery'}
-                onToogleAccordion={(event: React.KeyboardEvent | React.MouseEvent): void =>
-                  handleToogleAccordion(event, 'delivery')
-                }
-              >
-                <>
-                  <li className="footer__delivery-list-item">
-                    <a
-                      tabIndex={activeAccordion === 'delivery' ? 0 : -1}
-                      className="footer__delivery-list-link"
-                      href=""
-                    >
-                      Самовывоз
-                    </a>
-                  </li>
-                  <li className="footer__delivery-list-item">
-                    <a
-                      tabIndex={activeAccordion === 'delivery' ? 0 : -1}
-                      className="footer__delivery-list-link"
-                      href=""
-                    >
-                      Доставка курьером
-                    </a>
-                  </li>
-                  <li className="footer__delivery-list-item">
-                    <a
-                      tabIndex={activeAccordion === 'delivery' ? 0 : -1}
-                      className="footer__delivery-list-link"
-                      href=""
-                    >
-                      Доставка транспортной компанией
-                    </a>
-                  </li>
-                  <li className="footer__delivery-list-item">
-                    <a
-                      tabIndex={activeAccordion === 'delivery' ? 0 : -1}
-                      className="footer__delivery-list-link"
-                      href=""
-                    >
-                      Способ оплаты
-                    </a>
-                  </li>
-                </>
-              </Accordion>
-              <Accordion
-                sectionName="footer"
-                listName="services"
-                title="Гарантия и сервис"
-                isOpen={activeAccordion === 'services'}
-                onToogleAccordion={(event: React.KeyboardEvent | React.MouseEvent): void =>
-                  handleToogleAccordion(event, 'services')
-                }
-              >
-                <>
-                  <li className="footer__services-list-item">
-                    <a
-                      tabIndex={activeAccordion === 'services' ? 0 : -1}
-                      className="footer__services-list-link"
-                      href=""
-                    >
-                      Возврат/обмен
-                    </a>
-                  </li>
-                  <li className="footer__services-list-item">
-                    <a
-                      tabIndex={activeAccordion === 'services' ? 0 : -1}
-                      className="footer__services-list-link"
-                      href=""
-                    >
-                      Ремонт и услуги
-                    </a>
-                  </li>
-                  <li className="footer__services-list-item">
-                    <a
-                      tabIndex={activeAccordion === 'services' ? 0 : -1}
-                      className="footer__services-list-link"
-                      href=""
-                    >
-                      Сервисные центры
-                    </a>
-                  </li>
-                  <li className="footer__services-list-item">
-                    <a
-                      tabIndex={activeAccordion === 'services' ? 0 : -1}
-                      className="footer__services-list-link"
-                      href=""
-                    >
-                      Сопровождение обращений
-                    </a>
-                  </li>
-                </>
-              </Accordion>
+              {accordionListData.map(({ listName, title, items }) => (
+                <Accordion
+                  key={listName}
+                  sectionName="footer"
+                  listName={listName}
+                  title={title}
+                  isOpen={activeAccordion === listName}
+                  onToogleAccordion={(): void => handleToogleAccordion(listName)}
+                >
+                  {items.map((item, index) => (
+                    <li key={index} className={`footer__${listName}-list-item`}>
+                      <a className={`footer__${listName}-list-link" href={item.link}`} target="_blank">
+                        {item.label}
+                      </a>
+                    </li>
+                  ))}
+                </Accordion>
+              ))}
             </div>
           </div>
           <div className="footer__social-networks">
             <p className="footer__social-networks-heading">Мы в социальных сетях</p>
             <ul className="footer__icon-list">
-              <li className="footer__icon-list-item">
-                <a className="footer__icon-list-link footer__icon-list-link--whatsapp" href=""></a>
-              </li>
-              <li className="footer__icon-list-item">
-                <a className="footer__icon-list-link footer__icon-list-link--classmates" href=""></a>
-              </li>
-              <li className="footer__icon-list-item">
-                <a className="footer__icon-list-link footer__icon-list-link--vk" href=""></a>
-              </li>
-              <li className="footer__icon-list-item">
-                <a className="footer__icon-list-link  footer__icon-list-link--facebook" href=""></a>
-              </li>
+              {socialIconsListData.map(({ className, href, key }) => (
+                <li key={key} className="footer__icon-list-item">
+                  <a className={`footer__icon-list-link ${className}`} href={href} target="_blank"></a>
+                </li>
+              ))}
             </ul>
           </div>
         </div>
@@ -164,7 +76,7 @@ const Footer = (): ReactElement => {
         <ul className="footer__legal-list-info">
           <li className="footer__legal-list-info-item">© Все права защищены 2023</li>
           <li className="footer__legal-list-info-item">
-            <a className="footer__legal-list-info-link" href="">
+            <a className="footer__legal-list-info-link" href="#" target="_blank">
               Политика конфиденциальности
             </a>
           </li>
---
+++ src/components/Footer/linksData.tsx
@@ -0,0 +1,29 @@
+export const accordionListData = [
+  {
+    listName: 'delivery',
+    title: 'Доставка и оплата',
+    items: [
+      { label: 'Самовывоз', link: '#' },
+      { label: 'Доставка курьером', link: '#' },
+      { label: 'Доставка транспортной компанией', link: '#' },
+      { label: 'Способ оплаты', link: '#' },
+    ],
+  },
+  {
+    listName: 'services',
+    title: 'Гарантия и сервис',
+    items: [
+      { label: 'Возврат/обмен', link: '#' },
+      { label: 'Ремонт и услуги', link: '#' },
+      { label: 'Сервисные центры', link: '#' },
+      { label: 'Сопровождение обращений', link: '#' },
+    ],
+  },
+];
+
+export const socialIconsListData = [
+  { className: 'footer__icon-list-link--whatsapp', href: '#', key: 1 },
+  { className: 'footer__icon-list-link--classmates', href: '#', key: 2 },
+  { className: 'footer__icon-list-link--vk', href: '#', key: 3 },
+  { className: 'footer__icon-list-link--facebook', href: '#', key: 4 },
+];
