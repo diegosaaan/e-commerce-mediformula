ee0d7f445cd3cf7a8aa93ee92c08c832855cbbcd
EvgeniiyaR <tulpansik@mail.ru>
2023-08-11 02:57:48 +0300

    feat: add authinput component

---
+++ src/components/AuthInput/AuthInput.tsx
@@ -0,0 +1,58 @@
+import React, { ReactElement, useState } from 'react';
+import '@/components/AuthInput/AuthInput.scss';
+import { IPropsAuthInput } from '@/types/interfaces';
+
+const AuthInput = ({
+  type,
+  placeholder,
+  name,
+  htmlFor,
+  isInputPassword,
+  textError,
+  onChange,
+  value,
+}: IPropsAuthInput): ReactElement => {
+  const [isInputFocused, setIsInputFocused] = useState(false);
+  const [isShowPassword, setIsShowPassword] = useState(false);
+
+  const handleShowPassword = (): void => {
+    if (isShowPassword) {
+      setIsShowPassword(false);
+    } else {
+      setIsShowPassword(true);
+    }
+  };
+
+  return (
+    <>
+      <div className={`auth__container-field ${(isInputFocused || value) && 'auth__container-field_active'}`}>
+        <div className="auth__container-input">
+          <label className={`auth__label ${(isInputFocused || value) && 'auth__label_active'}`} htmlFor={htmlFor}>
+            {placeholder}
+          </label>
+          <input
+            className="auth__input"
+            id={htmlFor}
+            name={name}
+            type={isShowPassword ? 'text' : type}
+            placeholder={placeholder}
+            onFocus={(): void => setIsInputFocused(true)}
+            onBlur={(): void => setIsInputFocused(false)}
+            value={value}
+            onChange={onChange}
+          />
+        </div>
+        {isInputPassword && (isInputFocused || value) && (
+          <button
+            className={`auth__button-pass ${isShowPassword && value && 'auth__button-pass_active'}`}
+            type="button"
+            onClick={handleShowPassword}
+          ></button>
+        )}
+      </div>
+      <p className="auth__input-error">{textError}</p>
+    </>
+  );
+};
+
+export default AuthInput;
