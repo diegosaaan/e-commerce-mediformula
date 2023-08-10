b327d6206750e2e150f7a894c80ea4d82562c581
EvgeniiyaR <tulpansik@mail.ru>
2023-08-11 02:58:43 +0300

    fix: change input component

--- src/components/Input/Input.tsx
+++ src/components/Input/Input.tsx
@@ -2,11 +2,30 @@ import React, { ReactElement } from 'react';
 import '@/components/Input/Input.scss';
 import { IPropsInput } from '@/types/interfaces';
 
-const Input = ({ className, type, placeholder, name, onChange }: IPropsInput): ReactElement => {
+const Input = ({
+  className,
+  classNameLabel,
+  type,
+  placeholder,
+  title,
+  name,
+  checked,
+  children,
+  onChange,
+}: IPropsInput): ReactElement => {
   return (
-    <>
-      <input className={className} type={type} placeholder={placeholder} name={name} onChange={onChange} />
-    </>
+    <label className={classNameLabel}>
+      {title}
+      <input
+        className={className}
+        type={type}
+        placeholder={placeholder}
+        name={name}
+        onChange={onChange}
+        checked={checked}
+      />
+      {children}
+    </label>
   );
 };
 
