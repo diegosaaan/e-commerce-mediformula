cea9048453fca9fd8a3cd8f85df64fea02afdf7d
Dimkov Alexandr <dimkovaleks4@gmail.com>
2023-08-11 00:04:31 +0300

    feat: add brends data

---
+++ src/pages/Main/components/brends-section/brendsData.tsx
@@ -0,0 +1,43 @@
+const brendsData = [
+  {
+    link: 'https://bbalance.ru/',
+    classModifier: 'bbalance',
+    key: 'bbalance',
+  },
+  {
+    link: 'https://www.omron.com/',
+    classModifier: 'omron',
+    key: 'omron',
+  },
+  {
+    link: 'https://www.avon.com/',
+    classModifier: 'avon',
+    key: 'avon',
+  },
+  {
+    link: 'https://and.medtechpro.ru/',
+    classModifier: 'and',
+    key: 'and',
+  },
+  {
+    link: 'https://www.morris-health.com/',
+    classModifier: 'morris-health',
+  },
+  {
+    link: 'https://www.forgingnewlives.com/',
+    classModifier: 'forgingnew',
+    key: 'forgingnew',
+  },
+  {
+    link: 'https://www.cnbc.com/',
+    classModifier: 'cnbce',
+    key: 'cnbce',
+  },
+  {
+    link: 'https://www.abena.com/',
+    classModifier: 'abena',
+    key: 'abena',
+  },
+];
+
+export default brendsData;
