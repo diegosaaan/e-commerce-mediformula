ed4671f21191dcc2fc2b8ad5b78dcba4fad0b517
Dimkov Alexandr <dimkovaleks4@gmail.com>
2023-08-11 00:08:55 +0300

    feat: add brends section to main page

--- src/pages/Main/Main.tsx
+++ src/pages/Main/Main.tsx
@@ -1,8 +1,10 @@
 import React, { ReactElement } from 'react';
+import Brends from './components/brends-section/Brends';
 import MediaSection from './components/media-section/Media';
 
 const MainPage = (): ReactElement => (
   <div className="main-page">
+    <Brends />
     <MediaSection />
   </div>
 );
