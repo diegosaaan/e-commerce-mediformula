1876623eecf18baefac24bd0e7ff2ee90b6b2e63
Dimkov Alexandr <dimkovaleks4@gmail.com>
2023-08-11 10:44:35 +0300

    feat: update advantages section's styles

--- src/pages/Main/Main.tsx
+++ src/pages/Main/Main.tsx
@@ -2,11 +2,13 @@ import React, { ReactElement } from 'react';
 import Brends from './components/brends-section/Brends';
 import MediaSection from './components/media-section/Media';
 import Advantages from './components/advantages-section/Advantages';
+import Services from './components/services-section/Services';
 
 const MainPage = (): ReactElement => (
   <div className="main-page">
     <Brends />
     <Advantages />
+    <Services />
     <MediaSection />
   </div>
 );
