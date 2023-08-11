9504d8e274ceb6580982ec30ef7bcdb63d04ab71
EvgeniiyaR <tulpansik@mail.ru>
2023-08-11 15:51:22 +0300

    feat: add list address component

---
+++ src/pages/Registration/components/ListAddress/ListAddress.tsx
@@ -0,0 +1,32 @@
+import React, { ReactElement } from 'react';
+import Input from '@/components/Input/Input';
+import { IPropsListAddress } from '@/types/interfaces';
+
+const ListAddress = ({ addresses, name, onChange }: IPropsListAddress): ReactElement => {
+  return (
+    <ul className="auth__list-address">
+      {addresses.map((item, index) => (
+        <li className="auth__item-address" key={index}>
+          <Input
+            className="auth__input-address"
+            classNameLabel="auth__label-address"
+            type="radio"
+            name={name}
+            onChange={onChange}
+            checked={true}
+          >
+            <span className="auth__span"></span>
+            <ul className="auth__list-address-value">
+              <li>Страна: {item.country}</li>
+              <li>Город: {item.city}</li>
+              <li>Индекс: {item.index}</li>
+              <li>Улица: {item.street}</li>
+            </ul>
+          </Input>
+        </li>
+      ))}
+    </ul>
+  );
+};
+
+export default ListAddress;
