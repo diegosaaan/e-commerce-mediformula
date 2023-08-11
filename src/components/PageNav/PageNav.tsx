cbabb4c56b854ac6a468eb7dea66341b256be62f
EvgeniiyaR <tulpansik@mail.ru>
2023-08-11 03:02:35 +0300

    fix: change header navigation

--- src/components/PageNav/PageNav.tsx
+++ src/components/PageNav/PageNav.tsx
@@ -1,6 +1,6 @@
 import './PageNav.scss';
 import React, { FormEvent, ReactElement } from 'react';
-import { NavLink, useNavigate } from 'react-router-dom';
+import { NavLink, Link, useNavigate } from 'react-router-dom';
 import useAuth from '@/utils/hooks/useAuth';
 import logo from '@/assets/images/svg/header-logo.svg';
 import ProfileButton from '@/components/ProfileButton/ProfileButton';
@@ -8,17 +8,20 @@ import Button from '@/components/Button/Button';
 import Input from '@/components/Input/Input';
 import Form from '@/components/Form/Form';
 
+const setActive = ({ isActive }: { isActive: boolean }): string =>
+  isActive ? 'header__link header__link_active' : 'header__link';
+
 export default function PageNav(): ReactElement {
   const { isUserLoggedIn, signOut } = useAuth();
   const navigate = useNavigate();
 
   const handleLogOut = (): void => signOut(() => navigate('/'));
-  const handleLogin = (): void => navigate('/login');
-  const handleRegister = (): void => navigate('/registration');
-  const handleMainPage = (): void => navigate('/');
 
-  const handleExampleClick = (): void => {};
   const handleExampleSumbit = (e: FormEvent<HTMLFormElement>): void => {
+    e.preventDefault();
+  };
+
+  const handleExampleChange = (e: FormEvent<HTMLInputElement>): void => {
     e.preventDefault();
   };
 
@@ -26,133 +29,87 @@ export default function PageNav(): ReactElement {
     <>
       <nav>
         <ul className="header__nav">
-          {!isUserLoggedIn && (
-            <li>
-              <Button
-                className="header__button-nav header__button-nav_type_register"
-                type="button"
-                onClick={handleRegister}
-                text="Регистрация"
-              />
+          <ul className="header__nav-container-auth">
+            <li className="header__item-logo-mobile">
+              <Link className="header__logo-link" to="/">
+                <img className="header__logo-mobile" src={logo} alt="Logo" />
+              </Link>
             </li>
-          )}
-          {!isUserLoggedIn && (
-            <li>
-              <Button
-                className="header__button-nav header__button-nav_type_login"
-                type="button"
-                onClick={handleLogin}
-                text="Логин"
-              />
-            </li>
-          )}
-          {isUserLoggedIn && (
-            <li>
-              <Button
-                className="header__button-nav header__button-nav_type_login"
-                type="button"
-                onClick={handleLogOut}
-                text="Выйти"
-              />
-            </li>
-          )}
-          <li>
-            <ProfileButton />
-          </li>
-        </ul>
-      </nav>
-
-      {/* for mobile */}
-
-      <nav className="header__mobile">
-        <ul className="header__nav-mobile">
-          <li>
-            <NavLink to="/">
-              <img className="header__logo-mobile" src={logo} alt="Logo" />
-            </NavLink>
-          </li>
-          <ul className="header__nav-container header__nav-container_type_mobile">
             {!isUserLoggedIn && (
-              <li>
-                <Button
-                  className="header__link header__link_type_register"
-                  type="button"
-                  onClick={handleRegister}
-                  text="Регистрация"
-                />
-              </li>
-            )}
-            {!isUserLoggedIn && (
-              <li>
-                <Button
-                  className="header__link header__link_type_login"
-                  type="button"
-                  onClick={handleLogin}
-                  text="Логин"
-                />
-              </li>
+              <>
+                <li className="header__item-reg">
+                  <Link className="header__link" to="/registration">
+                    <Button
+                      className="header__button-nav header__button-nav_type_register"
+                      type="button"
+                      text="Регистрация"
+                    />
+                  </Link>
+                </li>
+                <li className="header__item-log">
+                  <Link className="header__link" to="/login">
+                    <Button className="header__button-nav header__button-nav_type_login" type="button" text="Войти" />
+                  </Link>
+                </li>
+              </>
             )}
             {isUserLoggedIn && (
-              <li>
-                <Button
-                  className="header__link header__link_type_login"
-                  type="button"
-                  onClick={handleLogOut}
-                  text="Выйти"
-                />
+              <li className="header__item-log">
+                <Link className="header__link" to="/">
+                  <Button
+                    className="header__button-nav header__button-nav_type_login"
+                    type="button"
+                    onClick={handleLogOut}
+                    text="Выйти"
+                  />
+                </Link>
               </li>
             )}
+            <li className="header__item-profile">
+              <Link className="header__link" to="/user-profile">
+                <ProfileButton />
+              </Link>
+            </li>
           </ul>
-        </ul>
-        <Form className="header__form-search" name="form-search" onSubmit={handleExampleSumbit}>
-          <Input
-            className="header__input-search-mobile"
-            name="header-search"
-            type="search"
-            placeholder="Поиск оборудования"
-            onChange={handleExampleClick}
-          />
-          <Button className="header__button-search" type="submit" />
-        </Form>
-      </nav>
-
-      <nav>
-        <ul className="header__nav-extra">
-          <ul className="header__nav-container">
-            <li>
-              <NavLink to="/">
+          <ul className="header__nav-container-main">
+            <li className="header__item-logo">
+              <Link className="header__logo-link" to="/">
                 <img className="header__logo" src={logo} alt="Logo" />
-              </NavLink>
+              </Link>
             </li>
-            <li>
-              <Button className="header__button-main" type="button" onClick={handleExampleClick} text="О нас" />
+            <li className="header__item-about">
+              <Link className="header__link" to="/about">
+                <Button className="header__button-main" type="button" text="О нас" />
+              </Link>
             </li>
-            <li>
-              <Button className="header__button-main" type="button" onClick={handleExampleClick}>
-                <div className="header__button-catalog">
-                  <div className="header__button-catalog-icon"></div>
-                  <p className="header__button-catalog-text">Каталог</p>
-                </div>
-              </Button>
+            <li className="header__item-catalog">
+              <Link className="header__link" to="/catalog">
+                <Button className="header__button-main" type="button">
+                  <div className="header__button-catalog">
+                    <div className="header__button-catalog-icon"></div>
+                    <p className="header__button-catalog-text">Каталог</p>
+                  </div>
+                </Button>
+              </Link>
             </li>
-          </ul>
-          <ul className="header__nav-container">
-            <li>
+            <li className="header__item-input-search">
               <Form className="header__form-search" name="form-search" onSubmit={handleExampleSumbit}>
                 <Input
                   className="header__input-search"
                   name="header-search"
                   type="search"
                   placeholder="Поиск оборудования"
-                  onChange={handleExampleClick}
+                  onChange={handleExampleChange}
                 />
                 <Button className="header__button-search" type="submit" />
               </Form>
             </li>
-            <li>
-              <Button className="header__button-cart" type="button" onClick={handleExampleClick}>
-                <p className="header__button-cart-text">3</p>
-              </Button>
+            <li className="header__item-cart">
+              <Link className="header__link" to="/cart">
+                <Button className="header__button-cart" type="button">
+                  <p className="header__button-cart-text">3</p>
+                </Button>
+              </Link>
             </li>
           </ul>
         </ul>
@@ -163,39 +120,45 @@ export default function PageNav(): ReactElement {
       <nav>
         <ul className="header__nav-extra-mobile">
           <li>
-            <Button className="header__button-main-mobile" type="button" onClick={handleMainPage}>
-              <div className="header__button-icon-mobile header__button-icon-mobile_type_main"></div>
-              <p className="header__button-text-mobile">Главная</p>
-            </Button>
+            <NavLink to="/" className={setActive}>
+              <Button className="header__button-main-mobile" type="button">
+                <div className="header__button-icon-mobile header__button-icon-mobile_type_main"></div>
+                <p className="header__button-text-mobile">Главная</p>
+              </Button>
+            </NavLink>
           </li>
           <li>
-            <Button className="header__button-main-mobile" type="button" onClick={handleExampleClick}>
-              <div className="header__button-icon-mobile header__button-icon-mobile_type_about"></div>
-              <p className="header__button-text-mobile">О нас</p>
-            </Button>
+            <NavLink to="/about" className={setActive}>
+              <Button className="header__button-main-mobile" type="button">
+                <div className="header__button-icon-mobile header__button-icon-mobile_type_about"></div>
+                <p className="header__button-text-mobile">О нас</p>
+              </Button>
+            </NavLink>
           </li>
           <li>
-            <Button className="header__button-main-mobile" type="button" onClick={handleExampleClick}>
-              <div className="header__button-icon-mobile header__button-catalog-icon-mobile_type_catalogue"></div>
-              <p className="header__button-text-mobile">Каталог</p>
-            </Button>
+            <NavLink to="/catalog" className={setActive}>
+              <Button className="header__button-main-mobile" type="button">
+                <div className="header__button-icon-mobile header__button-catalog-icon-mobile_type_catalogue"></div>
+                <p className="header__button-text-mobile">Каталог</p>
+              </Button>
+            </NavLink>
           </li>
           <li>
-            <Button
-              className="header__button-main-mobile header__button-main-mobile_type_cart"
-              type="button"
-              onClick={handleExampleClick}
-            >
-              <div className="header__button-icon-mobile header__button-cart-icon-mobile_type_cart"></div>
-              <p className="header__button-text-mobile">Корзина</p>
-              <p className="header__button-cart-text header__button-cart-text_type_mobile">3</p>
-            </Button>
+            <NavLink to="/cart" className={setActive}>
+              <Button className="header__button-main-mobile header__button-main-mobile_type_cart" type="button">
+                <div className="header__button-icon-mobile header__button-cart-icon-mobile_type_cart"></div>
+                <p className="header__button-text-mobile">Корзина</p>
+                <p className="header__button-cart-text header__button-cart-text_type_mobile">3</p>
+              </Button>
+            </NavLink>
           </li>
           <li>
-            <Button className="header__button-main-mobile" type="button" onClick={handleExampleClick}>
-              <div className="header__button-icon-mobile header__button-cart-icon-mobile_type_profile"></div>
-              <p className="header__button-text-mobile">Профиль</p>
-            </Button>
+            <NavLink to="/user-profile" className={setActive}>
+              <Button className="header__button-main-mobile" type="button">
+                <div className="header__button-icon-mobile header__button-cart-icon-mobile_type_profile"></div>
+                <p className="header__button-text-mobile">Профиль</p>
+              </Button>
+            </NavLink>
           </li>
         </ul>
       </nav>
