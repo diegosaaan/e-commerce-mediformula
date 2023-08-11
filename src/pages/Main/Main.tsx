013d07643f85068ce858327f893facd2872650e6
Dimkov Alexandr <dimkovaleks4@gmail.com>
2023-08-11 04:38:59 +0300

    refactor: change components order in main page

--- src/pages/Main/Main.tsx
+++ src/pages/Main/Main.tsx
@@ -1,10 +1,12 @@
 import React, { ReactElement } from 'react';
 import Brends from './components/brends-section/Brends';
 import MediaSection from './components/media-section/Media';
+import Advantages from './components/advantages-section/Advantages';
 
 const MainPage = (): ReactElement => (
   <div className="main-page">
     <Brends />
+    <Advantages />
     <MediaSection />
   </div>
 );
