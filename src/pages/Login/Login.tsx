194ec36712765d0946f6a26c78b5606529fb666c
EvgeniiyaR <tulpansik@mail.ru>
2023-08-11 03:04:07 +0300

    fix: change login component

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -1,7 +1,9 @@
 import '@/pages/Login/Login.scss';
 import React, { FormEvent, ReactElement } from 'react';
-import { Link, useLocation, useNavigate } from 'react-router-dom';
+import { useLocation, useNavigate } from 'react-router-dom';
 import useAuth from '@/utils/hooks/useAuth';
+import AuthInput from '@/components/AuthInput/AuthInput';
+import AuthForm from '@/components/AuthForm/AuthForm';
 
 function LoginPage(): ReactElement {
   const { signIn } = useAuth();
@@ -9,26 +11,30 @@ function LoginPage(): ReactElement {
   const location = useLocation();
   const fromPage = location.state?.from?.pathname || '/';
 
-  const goBack = (): void => navigate(-1);
-
-  const handleSubmit = (event: FormEvent<HTMLFormElement>): void => {
+  const handleLogin = (event: FormEvent<HTMLFormElement>): void => {
     event.preventDefault();
     signIn(() => navigate(fromPage));
   };
 
   return (
-    <div className="login-page">
-      <h1>Страница логина</h1>
-      <button onClick={goBack}>Вернуться назад</button>
-      <form action="" onSubmit={handleSubmit}>
-        <input type="text" placeholder="Введите email..." />
-        <input type="text" placeholder="Введите пароль..." />
-        <button type="submit">Войти</button>
-        <p>
-          Еще не зарегистрированы? <Link to="/registration">Зарегистрироваться</Link>
-        </p>
-      </form>
-    </div>
+    <AuthForm
+      onSubmit={handleLogin}
+      name="form-login"
+      text="Еще не зарегистрированы?"
+      textLink="Регистрация"
+      textButton="Войти"
+      path="/registration"
+      title="Вход"
+    >
+      <ul className="auth__list auth__list_active">
+        <li>
+          <AuthInput type="email" placeholder="Email*" name="email" htmlFor="email" isInputPassword={false} />
+        </li>
+        <li>
+          <AuthInput type="password" placeholder="Пароль*" name="password" htmlFor="password" isInputPassword={true} />
+        </li>
+      </ul>
+    </AuthForm>
   );
 }
 
