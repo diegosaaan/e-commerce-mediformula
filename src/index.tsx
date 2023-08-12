e6be3254019af6a57f6e523b1fb347af8e53e16e
Dimkov Alexandr <dimkovaleks4@gmail.com>
2023-08-12 12:11:42 +0300

    feat: add Router Provider to index.tsx file

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,16 @@
+import '@/index.scss';
+import '@/pages/App.scss';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from '@/pages/App';
-import '@/index.scss';
+import { RouterProvider } from 'react-router-dom';
+import router from './routes/router';
+import { AuthProvider } from './hoc/AuthProvider';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <AuthProvider>
+      <RouterProvider router={router} />
+    </AuthProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
