79b01b0056b27fa4aeb1011917d950d820f79999
EvgeniiyaR <tulpansik@mail.ru>
2023-08-14 00:45:04 +0300

    feat: add types for login, reg schemies

--- src/types/types.ts
+++ src/types/types.ts
@@ -4,3 +4,32 @@ export type AddressType = {
   index: string;
   street: string;
 };
+
+export type LoginSchemaType = {
+  email: string;
+  password: string;
+};
+
+export type RegisterSchemaType = {
+  firstName: string;
+  lastName: string;
+  date: string;
+  email: string;
+  password: string;
+  passwordRepeat: string;
+
+  shipping: string;
+  billing: string;
+
+  shippingCountry: string;
+  shippingCity: string;
+  shippingIndex: string;
+  shippingStreet: string;
+
+  billingCountry: string;
+  billingCity: string;
+  billingIndex: string;
+  billingStreet: string;
+};
+
+export type SetFieldValueType = (field: string, value: string, shouldValidate?: boolean) => void;
