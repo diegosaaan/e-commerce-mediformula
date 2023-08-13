a487f2eb5265eca0e85eec1532654ec524d3564c
EvgeniiyaR <tulpansik@mail.ru>
2023-08-14 00:41:52 +0300

    feat: add validation for registration

--- src/pages/Registration/Registration.tsx
+++ src/pages/Registration/Registration.tsx
@@ -1,44 +1,23 @@
-import React, { FormEvent, ReactElement, useState } from 'react';
+import React, { ReactElement, useState } from 'react';
 import '@/pages/Registration/Registration.scss';
+import { Formik } from 'formik';
 import AuthInput from '@/components/AuthInput/AuthInput';
 import AuthForm from '@/components/AuthForm/AuthForm';
-import { AddressType } from '@/types/types';
+import { AddressType, RegisterSchemaType, SetFieldValueType } from '@/types/types';
 import ListAddress from '@/pages/Registration/components/ListAddress/ListAddress';
 import AddressFields from '@/pages/Registration/components/AddressFields/AddressFields';
+import { RegisterSchema } from '@/utils/helpers/validationSchemes';
 
 const RegistrationPage = (): ReactElement => {
   const [isAddAddress, setIsAddAddress] = useState(false);
   const [isShippingAddress, setIsShippingAddress] = useState(false);
   const [isBillingAddress, setIsBillingAddress] = useState(false);
 
-  const [shippingAddress, setShippingAddress] = useState({
-    country: '',
-    city: '',
-    index: '',
-    street: '',
-  });
-
-  const [billingAddress, setBillingAddress] = useState({
-    country: '',
-    city: '',
-    index: '',
-    street: '',
-  });
-
-  const [info, setInfo] = useState({
-    firstname: '',
-    lastname: '',
-    date: '',
-    email: '',
-    password: '',
-    passwordrepeat: '',
-  });
-
   const [shippingAddresses, setShippingAddresses] = useState<AddressType[]>([]);
   const [billingAddresses, setBillingAddresses] = useState<AddressType[]>([]);
 
-  const handleRegister = (event: FormEvent<HTMLFormElement>): void => {
-    event.preventDefault();
+  const handleRegister = (values: RegisterSchemaType): void => {
+    console.log(values);
   };
 
   const handleAddAddresses = (): void => {
@@ -59,194 +38,280 @@ const RegistrationPage = (): ReactElement => {
     setIsBillingAddress(true);
   };
 
-  const handleAddShippingAddress = (): void => {
+  const handleAddShippingAddress = (values: RegisterSchemaType, setFieldValue: SetFieldValueType): void => {
     setIsShippingAddress(false);
-    setShippingAddresses((prevAddresses) => [...prevAddresses, shippingAddress]);
-    setShippingAddress({
-      country: '',
-      city: '',
-      index: '',
-      street: '',
-    });
+    setFieldValue('shippingCountry', '');
+    setFieldValue('shippingCity', '');
+    setFieldValue('shippingIndex', '');
+    setFieldValue('shippingStreet', '');
+    setShippingAddresses((prevAddresses) => [
+      ...prevAddresses,
+      {
+        country: values.shippingCountry,
+        city: values.shippingCity,
+        index: values.shippingIndex,
+        street: values.shippingStreet,
+      },
+    ]);
   };
 
-  const handleAddBillingAddress = (): void => {
+  const handleAddBillingAddress = (values: RegisterSchemaType, setFieldValue: SetFieldValueType): void => {
     setIsBillingAddress(false);
-    setBillingAddresses((prevAddresses) => [...prevAddresses, billingAddress]);
-    setBillingAddress({
-      country: '',
-      city: '',
-      index: '',
-      street: '',
-    });
-  };
-
-  const handleShippingChange = (e: FormEvent<HTMLInputElement>): void => {
-    e.preventDefault();
-    const target = e.target as HTMLInputElement;
-    const { name, value } = target;
-    setShippingAddress((prevState) => ({
-      ...prevState,
-      [name]: value,
-    }));
-  };
-
-  const handleBillingChange = (e: FormEvent<HTMLInputElement>): void => {
-    e.preventDefault();
-    const target = e.target as HTMLInputElement;
-    const { name, value } = target;
-    setBillingAddress((prevState) => ({
-      ...prevState,
-      [name]: value,
-    }));
-  };
-
-  const handleInfoChange = (e: FormEvent<HTMLInputElement>): void => {
-    e.preventDefault();
-    const target = e.target as HTMLInputElement;
-    const { name, value } = target;
-    setInfo((prevState) => ({
-      ...prevState,
-      [name]: value,
-    }));
-  };
-
-  const handleExampleChange = (e: FormEvent<HTMLInputElement>): void => {
-    e.preventDefault();
+    setFieldValue('billingCountry', '');
+    setFieldValue('billingCity', '');
+    setFieldValue('billingIndex', '');
+    setFieldValue('billingStreet', '');
+    setBillingAddresses((prevAddresses) => [
+      ...prevAddresses,
+      {
+        country: values.billingCountry,
+        city: values.billingCity,
+        index: values.billingIndex,
+        street: values.billingStreet,
+      },
+    ]);
   };
 
   return (
-    <AuthForm
+    <Formik
+      initialValues={{
+        firstName: '',
+        lastName: '',
+        date: '',
+        email: '',
+        password: '',
+        passwordRepeat: '',
+        shipping: '',
+        billing: '',
+        shippingCountry: '',
+        shippingCity: '',
+        shippingIndex: '',
+        shippingStreet: '',
+        billingCountry: '',
+        billingCity: '',
+        billingIndex: '',
+        billingStreet: '',
+      }}
+      validationSchema={RegisterSchema}
       onSubmit={handleRegister}
-      name="form-register"
-      text="Уже зарегистрированы?"
-      textLink="Войти"
-      textButton="Зарегистрироваться"
-      path="/login"
-      title="Регистрация"
-      isRegister={true}
-      disabled={isAddAddress && true}
-      isAddAddress={isAddAddress}
-      handlePrevRegister={handlePrevRegister}
     >
-      <ul className={`auth__list auth__list_type_main ${isAddAddress ? 'auth__list_inactive' : 'auth__list_active'}`}>
-        <li>
-          <AuthInput
-            type="text"
-            placeholder="Имя*"
-            name="firstname"
-            htmlFor="firstname"
-            isInputPassword={false}
-            onChange={handleInfoChange}
-            value={info.firstname}
-          />
-        </li>
-        <li>
-          <AuthInput
-            type="text"
-            placeholder="Фамилия*"
-            name="lastname"
-            htmlFor="lastname"
-            isInputPassword={false}
-            onChange={handleInfoChange}
-            value={info.lastname}
-          />
-        </li>
-        <li>
-          <AuthInput
-            type="date"
-            placeholder="Дата рождения*"
-            name="date"
-            htmlFor="date"
-            isInputPassword={false}
-            onChange={handleInfoChange}
-            value={info.date}
-          />
-        </li>
-        <li>
-          <AuthInput
-            type="email"
-            placeholder="Email*"
-            name="email"
-            htmlFor="email"
-            isInputPassword={false}
-            textError="Неправильный email"
-            onChange={handleInfoChange}
-            value={info.email}
-          />
-        </li>
-        <li>
-          <AuthInput
-            type="password"
-            placeholder="Пароль*"
-            name="password"
-            htmlFor="password"
-            isInputPassword={true}
-            onChange={handleInfoChange}
-            value={info.password}
-          />
-        </li>
-        <li>
-          <AuthInput
-            type="password"
-            placeholder="Повторите пароль*"
-            name="passwordrepeat"
-            htmlFor="passwordrepeat"
-            isInputPassword={true}
-            onChange={handleInfoChange}
-            value={info.passwordrepeat}
-          />
-        </li>
-      </ul>
+      {({ values, errors, touched, handleChange, dirty, setFieldValue }): JSX.Element => (
+        <AuthForm
+          name="form-register"
+          text="Уже зарегистрированы?"
+          textLink="Войти"
+          textButton="Зарегистрироваться"
+          path="/login"
+          title="Регистрация"
+          isRegister={true}
+          disabled={
+            !dirty ||
+            !!(
+              errors.firstName ||
+              errors.lastName ||
+              errors.date ||
+              errors.email ||
+              errors.password ||
+              errors.passwordRepeat ||
+              errors.shipping ||
+              errors.billing
+            )
+          }
+          isAddAddress={isAddAddress}
+          handlePrevRegister={handlePrevRegister}
+        >
+          <ul
+            className={`auth__list auth__list_type_main ${isAddAddress ? 'auth__list_inactive' : 'auth__list_active'}`}
+          >
+            <li>
+              <AuthInput
+                type="text"
+                placeholder="Имя*"
+                name="firstName"
+                htmlFor="firstName"
+                isInputPassword={false}
+                onChange={handleChange}
+                value={values.firstName}
+                errors={errors.firstName}
+                touched={touched.firstName}
+              />
+            </li>
+            <li>
+              <AuthInput
+                type="text"
+                placeholder="Фамилия*"
+                name="lastName"
+                htmlFor="lastName"
+                isInputPassword={false}
+                onChange={handleChange}
+                value={values.lastName}
+                errors={errors.lastName}
+                touched={touched.lastName}
+              />
+            </li>
+            <li>
+              <AuthInput
+                type="date"
+                placeholder="Дата рождения*"
+                name="date"
+                htmlFor="date"
+                isInputPassword={false}
+                onChange={handleChange}
+                value={values.date}
+                errors={errors.date}
+                touched={touched.date}
+              />
+            </li>
+            <li>
+              <AuthInput
+                type="email"
+                placeholder="Email*"
+                name="email"
+                htmlFor="email"
+                isInputPassword={false}
+                onChange={handleChange}
+                value={values.email}
+                errors={errors.email}
+                touched={touched.email}
+              />
+            </li>
+            <li>
+              <AuthInput
+                type="password"
+                placeholder="Пароль*"
+                name="password"
+                htmlFor="password"
+                isInputPassword={true}
+                onChange={handleChange}
+                value={values.password}
+                errors={errors.password}
+                touched={touched.password}
+              />
+            </li>
+            <li>
+              <AuthInput
+                type="password"
+                placeholder="Повторите пароль*"
+                name="passwordRepeat"
+                htmlFor="passwordRepeat"
+                isInputPassword={true}
+                onChange={handleChange}
+                value={values.passwordRepeat}
+                errors={errors.passwordRepeat}
+                touched={touched.passwordRepeat}
+              />
+            </li>
+          </ul>
 
-      {!isAddAddress && (
-        <>
-          <button className="auth__button auth__button_type_add" type="button" onClick={handleAddAddresses}>
-            Добавить адреса
-          </button>
-        </>
+          {!isAddAddress && (
+            <>
+              <button className="auth__button auth__button_type_add" type="button" onClick={handleAddAddresses}>
+                Добавить адреса
+              </button>
+            </>
+          )}
+
+          <ul className={`auth__list ${isAddAddress ? 'auth__list_active' : 'auth__list_inactive'}`}>
+            <li>
+              <h2 className="auth__subtitle">Добавьте адреса для доставок и выберите адрес по умолчанию</h2>
+              {!isShippingAddress && (
+                <>
+                  <button className="auth__button-add" type="button" onClick={handleOpenAddressShipping}></button>
+                  <ListAddress name="shipping" onChange={handleChange} addresses={shippingAddresses} />
+                </>
+              )}
+              <p className="auth__input-error">{errors.shipping && errors.shipping}</p>
+            </li>
+
+            {isShippingAddress && (
+              <>
+                <AddressFields
+                  onChange={handleChange}
+                  name="shipping"
+                  country={values.shippingCountry}
+                  city={values.shippingCity}
+                  index={values.shippingIndex}
+                  street={values.shippingStreet}
+                  countryErrors={errors.shippingCountry}
+                  cityErrors={errors.shippingCity}
+                  indexErrors={errors.shippingIndex}
+                  streetErrors={errors.shippingStreet}
+                  countryTouched={touched.shippingCountry}
+                  cityTouched={touched.shippingCity}
+                  indexTouched={touched.shippingIndex}
+                  streetTouched={touched.shippingStreet}
+                />
+                <button
+                  className="auth__button auth__button_type_add"
+                  type="button"
+                  disabled={
+                    !!(
+                      errors.shippingCity ||
+                      errors.shippingCountry ||
+                      errors.shippingIndex ||
+                      errors.shippingStreet
+                    ) ||
+                    !values.shippingCity ||
+                    !values.shippingCountry ||
+                    !values.shippingIndex ||
+                    !values.shippingStreet
+                  }
+                  onClick={(): void => handleAddShippingAddress(values, setFieldValue)}
+                >
+                  Добавить
+                </button>
+              </>
+            )}
+
+            <li>
+              <h2 className="auth__subtitle">Добавьте адрес для выставления счетов и выберите адрес по умолчанию</h2>
+              {!isBillingAddress && (
+                <>
+                  <button className="auth__button-add" type="button" onClick={handleOpenAddressBilling}></button>
+                  <ListAddress name="billing" onChange={handleChange} addresses={billingAddresses} />
+                </>
+              )}
+              <p className="auth__input-error">{errors.billing && errors.billing}</p>
+            </li>
+
+            {isBillingAddress && (
+              <>
+                <AddressFields
+                  onChange={handleChange}
+                  name="billing"
+                  country={values.billingCountry}
+                  city={values.billingCity}
+                  index={values.billingIndex}
+                  street={values.billingStreet}
+                  countryErrors={errors.billingCountry}
+                  cityErrors={errors.billingCity}
+                  indexErrors={errors.billingIndex}
+                  streetErrors={errors.billingStreet}
+                  countryTouched={touched.billingCountry}
+                  cityTouched={touched.billingCity}
+                  indexTouched={touched.billingIndex}
+                  streetTouched={touched.billingStreet}
+                />
+                <button
+                  className="auth__button auth__button_type_add"
+                  type="button"
+                  disabled={
+                    !!(errors.billingCity || errors.billingCountry || errors.billingIndex || errors.billingStreet) ||
+                    !values.billingCity ||
+                    !values.billingCountry ||
+                    !values.billingIndex ||
+                    !values.billingStreet
+                  }
+                  onClick={(): void => handleAddBillingAddress(values, setFieldValue)}
+                >
+                  Добавить
+                </button>
+              </>
+            )}
+          </ul>
+        </AuthForm>
       )}
-
-      <ul className={`auth__list ${isAddAddress ? 'auth__list_active' : 'auth__list_inactive'}`}>
-        <li>
-          <h2 className="auth__subtitle">Добавьте адреса для доставок и выберите адрес по умолчанию</h2>
-          {!isShippingAddress && (
-            <>
-              <button className="auth__button-add" type="button" onClick={handleOpenAddressShipping}></button>
-              <ListAddress name="shipping" onChange={handleExampleChange} addresses={shippingAddresses} />
-            </>
-          )}
-        </li>
-
-        {isShippingAddress && (
-          <>
-            <AddressFields address={shippingAddress} onChange={handleShippingChange} />
-            <button className="auth__button auth__button_type_add" type="button" onClick={handleAddShippingAddress}>
-              Добавить
-            </button>
-          </>
-        )}
-
-        <li>
-          <h2 className="auth__subtitle">Добавьте адрес для выставления счетов и выберите адрес по умолчанию</h2>
-          {!isBillingAddress && (
-            <>
-              <button className="auth__button-add" type="button" onClick={handleOpenAddressBilling}></button>
-              <ListAddress name="billing" onChange={handleExampleChange} addresses={billingAddresses} />
-            </>
-          )}
-        </li>
-
-        {isBillingAddress && (
-          <>
-            <AddressFields address={billingAddress} onChange={handleBillingChange} />
-            <button className="auth__button auth__button_type_add" type="button" onClick={handleAddBillingAddress}>
-              Добавить
-            </button>
-          </>
-        )}
-      </ul>
-    </AuthForm>
+    </Formik>
   );
 };
 
