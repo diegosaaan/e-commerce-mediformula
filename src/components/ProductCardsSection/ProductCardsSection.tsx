2e5a32a861efb1cf72ec781ab671ef8c7cd27ed5
Dimkov Alexandr <dimkovaleks4@gmail.com>
2023-08-16 10:43:31 +0300

    feat: add swiper to product card section

--- src/components/ProductCardsSection/ProductCardsSection.tsx
+++ src/components/ProductCardsSection/ProductCardsSection.tsx
@@ -1,25 +1,21 @@
-import React, { ReactElement, useLayoutEffect, useRef, useState } from 'react';
-import '@/components/ProductCardsSection/ProductCardsSection.scss';
-import '@/pages/App.scss';
+import './ProductCardsSection.scss';
+import React, { ReactElement, useRef } from 'react';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Navigation } from 'swiper';
+import { Link } from 'react-router-dom';
 import Card from '@/components/Card/Card';
 import imagePath from '@/assets/images/png/card-product-image-example.png';
 import { IPropsCardsSection } from '@/types/interfaces';
-import Button from '@/components/Button/Button';
-import Carousel from '../Carousel/Carousel';
+import arrowRightPath from '@/assets/images/svg/arrow-ahead.svg';
+import arrowLeftPath from '@/assets/images/svg/arrow-back.svg';
+
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/free-mode';
 
 const ProductCardsSection = ({ header, counter }: IPropsCardsSection): ReactElement => {
-  console.log('I am');
-  const cardRef = useRef<HTMLDivElement | null>(null);
-  const [cardWidth, setCardWidth] = useState(0);
-
-  useLayoutEffect(() => {
-    console.log('useLayoutEffect is running');
-    if (cardRef.current) {
-      const width = cardRef.current.offsetWidth;
-      console.log('Width inside useLayoutEffect:', width);
-      setCardWidth(width);
-    }
-  }, []);
+  const prevRef = useRef(null);
+  const nextRef = useRef(null);
 
   return (
     <section className="_container cardsSection">
@@ -28,139 +24,154 @@ const ProductCardsSection = ({ header, counter }: IPropsCardsSection): ReactElem
           {header}
           <sup className="cardsSection__counter">{counter}</sup>
         </h2>
-        <Button
-          className="textButton"
-          type="button"
-          text="⟶ Смотреть все"
-          onClick={(): void => {
-            console.log('Clicked Look here!');
-          }}
-        />
+        <Link className="textButton" to="/catalog">
+          ⟶ Смотреть все
+        </Link>
       </div>
       <div className="cardsSection__cards-container">
-        <Carousel cardWidth={cardWidth}>
-          <Card
-            ref={cardRef}
-            imagePath={imagePath}
-            rating={4.8}
-            text="1Кислородный концентратор JAY-10 двухпоточный"
-            price={81938}
-            priceBefore={85348}
-            bonus={450}
-            discount={10}
-            onClick={(): void => {
-              console.log('Clicked!');
-            }}
-          />
-          <Card
-            imagePath={imagePath}
-            rating={4.8}
-            text="2Кислородный концентратор JAY-10 двухпоточный"
-            price={81938}
-            priceBefore={85348}
-            bonus={450}
-            discount={10}
-            onClick={(): void => {
-              console.log('Clicked!');
-            }}
-          />
-          <Card
-            imagePath={imagePath}
-            rating={4.8}
-            text="3Кислородный концентратор JAY-10 двухпоточный"
-            price={81938}
-            priceBefore={85348}
-            bonus={450}
-            discount={10}
-            onClick={(): void => {
-              console.log('Clicked!');
-            }}
-          />
-          <Card
-            imagePath={imagePath}
-            rating={4.8}
-            text="4Кислородный концентратор JAY-10 двухпоточный"
-            price={81938}
-            priceBefore={85348}
-            bonus={450}
-            discount={10}
-            onClick={(): void => {
-              console.log('Clicked!');
-            }}
-          />
-          <Card
-            imagePath={imagePath}
-            rating={4.8}
-            text="5Кислородный концентратор JAY-10 двухпоточный"
-            price={81938}
-            priceBefore={85348}
-            bonus={450}
-            discount={10}
-            onClick={(): void => {
-              console.log('Clicked!');
-            }}
-          />
-          <Card
-            imagePath={imagePath}
-            rating={4.8}
-            text="6Кислородный концентратор JAY-10 двухпоточный"
-            price={81938}
-            priceBefore={85348}
-            bonus={450}
-            discount={10}
-            onClick={(): void => {
-              console.log('Clicked!');
-            }}
-          />
-          <Card
-            imagePath={imagePath}
-            rating={4.8}
-            text="Кислородный концентратор JAY-10 двухпоточный"
-            price={81938}
-            priceBefore={85348}
-            bonus={450}
-            discount={10}
-            onClick={(): void => {
-              console.log('Clicked!');
-            }}
-          />
-          <Card
-            imagePath={imagePath}
-            rating={4.8}
-            text="Кислородный концентратор JAY-10 двухпоточный"
-            price={81938}
-            priceBefore={85348}
-            bonus={450}
-            discount={10}
-            onClick={(): void => {
-              console.log('Clicked!');
-            }}
-          />
-          <Card
-            imagePath={imagePath}
-            rating={4.8}
-            text="Кислородный концентратор JAY-10 двухпоточный"
-            price={81938}
-            priceBefore={85348}
-            bonus={450}
-            discount={10}
-            onClick={(): void => {
-              console.log('Clicked!');
-            }}
-          />
-          <Card
-            imagePath={imagePath}
-            rating={4.8}
-            text="Кислородный концентратор JAY-10 двухпоточный"
-            price={81938}
-            priceBefore={85348}
-            bonus={450}
-            discount={10}
-            onClick={(): void => {
-              console.log('Clicked!');
-            }}
-          />
-        </Carousel>
+        <button className="arrow arrow--prev">
+          <img src={arrowLeftPath} ref={prevRef} alt="Shevron Left" />
+        </button>
+        <Swiper
+          modules={[Navigation]}
+          loop={true}
+          slidesPerView={4}
+          navigation={{
+            prevEl: '.arrow--prev',
+            nextEl: '.arrow--next',
+          }}
+          breakpoints={{
+            320: {
+              slidesPerView: 1,
+            },
+            567: {
+              slidesPerView: 2,
+            },
+            767: {
+              slidesPerView: 2.5,
+            },
+            1023: {
+              slidesPerView: 3,
+            },
+            1279: {
+              slidesPerView: 4,
+            },
+          }}
+        >
+          <SwiperSlide>
+            <Card
+              imagePath={imagePath}
+              rating={4.8}
+              text="1Кислородный концентратор JAY-10 двухпоточный"
+              price={81938}
+              priceBefore={85348}
+              bonus={450}
+              discount={10}
+            />
+          </SwiperSlide>
+          <SwiperSlide>
+            <Card
+              imagePath={imagePath}
+              rating={4.8}
+              text="1Кислородный концентратор JAY-10 двухпоточный"
+              price={81938}
+              priceBefore={85348}
+              bonus={450}
+              discount={10}
+            />
+          </SwiperSlide>
+          <SwiperSlide>
+            <Card
+              imagePath={imagePath}
+              rating={4.8}
+              text="1Кислородный концентратор JAY-10 двухпоточный"
+              price={81938}
+              priceBefore={85348}
+              bonus={450}
+              discount={10}
+            />
+          </SwiperSlide>
+          <SwiperSlide>
+            <Card
+              imagePath={imagePath}
+              rating={4.8}
+              text="1Кислородный концентратор JAY-10 двухпоточный"
+              price={81938}
+              priceBefore={85348}
+              bonus={450}
+              discount={10}
+            />
+          </SwiperSlide>
+          <SwiperSlide>
+            <Card
+              imagePath={imagePath}
+              rating={4.8}
+              text="1Кислородный концентратор JAY-10 двухпоточный"
+              price={81938}
+              priceBefore={85348}
+              bonus={450}
+              discount={10}
+            />
+          </SwiperSlide>
+          <SwiperSlide>
+            <Card
+              imagePath={imagePath}
+              rating={4.8}
+              text="1Кислородный концентратор JAY-10 двухпоточный"
+              price={81938}
+              priceBefore={85348}
+              bonus={450}
+              discount={10}
+            />
+          </SwiperSlide>
+          <SwiperSlide>
+            <Card
+              imagePath={imagePath}
+              rating={4.8}
+              text="1Кислородный концентратор JAY-10 двухпоточный"
+              price={81938}
+              priceBefore={85348}
+              bonus={450}
+              discount={10}
+            />
+          </SwiperSlide>
+          <SwiperSlide>
+            <Card
+              imagePath={imagePath}
+              rating={4.8}
+              text="1Кислородный концентратор JAY-10 двухпоточный"
+              price={81938}
+              priceBefore={85348}
+              bonus={450}
+              discount={10}
+            />
+          </SwiperSlide>
+          <SwiperSlide>
+            <Card
+              imagePath={imagePath}
+              rating={4.8}
+              text="1Кислородный концентратор JAY-10 двухпоточный"
+              price={81938}
+              priceBefore={85348}
+              bonus={450}
+              discount={10}
+            />
+          </SwiperSlide>
+          <SwiperSlide>
+            <Card
+              imagePath={imagePath}
+              rating={4.8}
+              text="1Кислородный концентратор JAY-10 двухпоточный"
+              price={81938}
+              priceBefore={85348}
+              bonus={450}
+              discount={10}
+            />
+          </SwiperSlide>
+        </Swiper>
+        <button ref={nextRef} className="arrow arrow--next">
+          <img src={arrowRightPath} alt="Shevron Right" />
+        </button>
       </div>
     </section>
   );
