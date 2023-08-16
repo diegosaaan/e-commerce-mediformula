193adba2f4be311d32def3f2fce8cdc06b2ee43d
Dimkov Alexandr <dimkovaleks4@gmail.com>
2023-08-16 19:16:59 +0300

    feat: add country select and postal index input

--- src/pages/Registration/components/AddressFields/AddressFields.tsx
+++ src/pages/Registration/components/AddressFields/AddressFields.tsx
@@ -1,46 +1,103 @@
-import '@/pages/Registration/components/AddressFields/AddressFields.scss';
-import React, { ReactElement } from 'react';
+import './AddressFields.scss';
+import React, { MouseEvent, KeyboardEvent, ReactElement, useState, useEffect, FormEvent } from 'react';
 import AuthInput from '@/components/AuthInput/AuthInput';
 import { IPropsAddressFields } from '@/types/interfaces';
+import Accordion from '@/components/Accordion/Accrodion';
+import selectData from '../fieldsData/selectData';
 
 const AddressFields = ({
   name,
   onChange,
-  country,
   city,
-  index,
   street,
-  countryErrors,
   cityErrors,
-  indexErrors,
   streetErrors,
-  countryTouched,
   cityTouched,
-  indexTouched,
+  setIsPostalCodeError,
   streetTouched,
+  accordionTitle,
+  setAcordionTitle,
+  postalCodeValue,
+  setPostalCodeValue,
 }: IPropsAddressFields): ReactElement => {
+  const [isAccordionOpen, setIsAccordionOpen] = useState(false);
+  const [errorMessage, setErrorMessage] = useState('Поле обязательно к заполнению');
+
+  const handlePostalCodeError = (): void => {
+    if (!postalCodeValue) {
+      setErrorMessage('Поле обязательно к заполнению');
+      return;
+    }
+
+    const matchingItem = selectData.find(({ ruName }) => ruName === accordionTitle);
+
+    setErrorMessage(
+      matchingItem && matchingItem.postalCode.test(postalCodeValue)
+        ? ''
+        : `${matchingItem?.errorMessage}, где Х - цифра` || ''
+    );
+  };
+
+  const handlePostalCodeValue = (event: FormEvent): void => {
+    const eventTarget = event.target as HTMLInputElement;
+    setPostalCodeValue(eventTarget.value);
+  };
+
+  const handleChooseAccordionItem = (event: MouseEvent | KeyboardEvent): void => {
+    const target = event.target as HTMLLIElement;
+
+    if (target && target.textContent) {
+      if (event.type === 'keydown' && (event as KeyboardEvent).key === 'Enter') {
+        setAcordionTitle(target.textContent);
+        setIsAccordionOpen(!isAccordionOpen);
+      } else if (event.type === 'click') {
+        setAcordionTitle(target.textContent);
+        setIsAccordionOpen(!isAccordionOpen);
+      }
+    }
+  };
+
+  const handleClickAccordion = (): void => {
+    setIsAccordionOpen(!isAccordionOpen);
+  };
+
+  const handleKeydownAccordion = (event: KeyboardEvent): void => {
+    if (event.key === 'Enter') {
+      setIsAccordionOpen(!isAccordionOpen);
+    }
+  };
+
+  useEffect(() => {
+    handlePostalCodeError();
+    setIsPostalCodeError(!!errorMessage);
+  }, [postalCodeValue, accordionTitle, errorMessage]);
+
   return (
     <ul className="auth__list auth__list_active">
       <li>
-        {/* <AuthInput
-          type="text"
-          placeholder="Страна*"
-          name={`${name}Country`}
-          htmlFor={`${name}Country`}
-          onChange={onChange}
-          value={country}
-          errors={countryErrors}
-          touched={countryTouched}
-        /> */}
-        <select className="auth__select" name={`${name}Country`}>
-          <option value={country}>Австрия</option>
-          <option value={country}>Германия</option>
-          <option value={country}>Франция</option>
-          <option value={country}>США</option>
-          <option value={country}>Польша</option>
-          <option value={country}>Бельгия</option>
-        </select>
-        <p className="auth__input-error">{countryErrors ? countryErrors || countryTouched : ''}</p>
+        <Accordion
+          sectionName="auth"
+          listName="select"
+          title={accordionTitle}
+          onClickAccordion={handleClickAccordion}
+          onKeydownAccordion={handleKeydownAccordion}
+          isOpen={isAccordionOpen}
+        >
+          {selectData
+            .filter(({ ruName }) => accordionTitle !== ruName)
+            .map(({ ruName, engName }) => (
+              <li
+                className="auth__select-list-item"
+                value={engName}
+                key={engName}
+                tabIndex={isAccordionOpen ? 0 : -1}
+                onClick={(event: MouseEvent): void => handleChooseAccordionItem(event)}
+                onKeyDown={(event: KeyboardEvent): void => handleChooseAccordionItem(event)}
+              >
+                {ruName}
+              </li>
+            ))}
+        </Accordion>
       </li>
       <li>
         <AuthInput
@@ -60,11 +117,10 @@ const AddressFields = ({
           placeholder="Почтовый индекс*"
           name={`${name}Index`}
           htmlFor={`${name}Index`}
-          onChange={onChange}
-          value={index}
-          errors={indexErrors}
-          touched={indexTouched}
+          onChange={handlePostalCodeValue}
+          value={postalCodeValue}
         />
+        <p className="auth__input-error">{errorMessage}</p>
       </li>
       <li>
         <AuthInput
