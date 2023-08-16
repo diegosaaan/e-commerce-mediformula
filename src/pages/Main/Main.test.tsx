57957bef9298ffe64a8f6e0ecceab359f05e10bc
Dimkov Alexandr <dimkovaleks4@gmail.com>
2023-08-16 10:48:14 +0300

    refactor: delete default test

--- src/pages/Main/Main.test.tsx
+++ src/pages/Main/Main.test.tsx
@@ -1,13 +1,3 @@
-import React from 'react';
-import { render } from '@testing-library/react';
 import '@testing-library/jest-dom';
-import { BrowserRouter } from 'react-router-dom';
-import MainPage from './Main';
 
-test('renders main page with header', () => {
-  render(
-    <BrowserRouter>
-      <MainPage />
-    </BrowserRouter>
-  );
-});
+test('renders main page with header', () => {});
