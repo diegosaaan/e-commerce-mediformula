24139f8142e0430aebc57c3d9542a767311a6b37
Dimkov Alexandr <dimkovaleks4@gmail.com>
2023-08-17 23:27:37 +0300

    feat: add default tests

--- src/pages/Main/Main.test.tsx
+++ src/pages/Main/Main.test.tsx
@@ -1,3 +1,17 @@
 import '@testing-library/jest-dom';
+// import React from 'react';
+// import { render } from 'react-dom';
+// import MainPage from './Main';
+// import RegistrationPage from '../Registration/Registration';
+// import LoginPage from '../Login/Login';
 
-test('renders main page with header', () => {});
+test('renders main page with header', () => {
+  // render(
+  //   <React.StrictMode>
+  //     <LoginPage />
+  //     <RegistrationPage />
+  //     <MainPage />
+  //   </React.StrictMode>,
+  //   document.getElementById('root')
+  // );
+});
