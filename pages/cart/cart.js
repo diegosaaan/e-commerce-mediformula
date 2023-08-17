a9998707d29ff88b5f3f087fcbe39e8d698335e6
Dimkov Alexandr <dimkovaleks4@gmail.com>
2023-08-17 19:03:09 +0300

    feat: upload sprint_2 build v1

---
+++ pages/cart/cart.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunke_commerce=self.webpackChunke_commerce||[]).push([[624],{4999:(e,c,t)=>{t.r(c),t.d(c,{default:()=>r});var a=t(7294);const r=()=>a.createElement("div",{className:"cart-page"},a.createElement("h1",{style:{textAlign:"center"}},"Страница корзины (в разработке)"),";")}}]);
