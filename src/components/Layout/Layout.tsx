5a4a0cf80464c21083f8897347bc657d333da493
Dimkov Alexandr <dimkovaleks4@gmail.com>
2023-08-18 23:43:28 +0300

    refactor: delete useEffect in Layout

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,32 +1,9 @@
-import React, { ReactElement, useEffect } from 'react';
-import { Outlet, useNavigate } from 'react-router-dom';
+import React, { ReactElement } from 'react';
+import { Outlet } from 'react-router-dom';
 import Header from '../Header/Header';
 import Footer from '../Footer/Footer';
-import * as userAuth from '@/services/auth-user';
-import useAuth from '@/utils/hooks/useAuth';
 
 export default function Layout(): ReactElement {
-  const { signIn, signOut } = useAuth();
-  const navigate = useNavigate();
-
-  useEffect(() => {
-    if (localStorage.getItem('token')) {
-      userAuth
-        .getUserInfo()
-        .then((res) => {
-          if (res) {
-            signIn(() => navigate('/'));
-          } else {
-            signOut(() => navigate('/'));
-          }
-        })
-        .catch((err) => {
-          signOut(() => navigate('/'));
-          console.log(`Возникла ошибка: ${err}`);
-        });
-    }
-  }, []);
-
   return (
     <>
       <Header />
