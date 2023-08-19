4c62c67f3d6141e32c86c10cdbeb7f4102220cf2
EvgeniiyaR <tulpansik@mail.ru>
2023-08-19 21:57:35 +0300

    feat: create headers for admin

--- src/services/headers.tsx
+++ src/services/headers.tsx
@@ -1,17 +1,20 @@
-import { getUserToken } from './tokenHelpers';
+import { getUserToken, getAdminToken } from './tokenHelpers';
 
-export const TOKEN = '4IpVxZKIXf0xJnkLBi7aJrhG1x_IW0GV';
 export const CLIENT_ID = 'ilGjRPUuSz9INMW6zL1pqJtl';
 export const CLIENT_SECRET = 'hhIZg0aLIDghQAX2aSCyqZ3kZhEsd50K';
-
-export const JSON_HEADERS = {
-  'Content-Type': 'application/json',
-  Authorization: `Bearer ${TOKEN}`,
-};
 
 export const URLENCODED_HEADERS = {
   'Content-Type': 'application/x-www-form-urlencoded',
   Authorization: `Basic ${btoa(`${CLIENT_ID}:${CLIENT_SECRET}`)}`,
+};
+
+export const createAdminJSONHeaders = async (): Promise<Record<string, string>> => {
+  const adminToken = await getAdminToken();
+
+  return {
+    'Content-Type': 'application/json',
+    Authorization: `Bearer ${adminToken}`,
+  };
 };
 
 export const createUserJSONHeaders = async (): Promise<Record<string, string>> => {
