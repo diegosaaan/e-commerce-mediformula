18998ed6413ac6732eb3bc6f1c788cb4fefe82f8
Dimkov Alexandr <dimkovaleks4@gmail.com>
2023-08-19 19:38:06 +0300

    feat: add some test

--- src/pages/Main/Main.test.tsx
+++ src/pages/Main/Main.test.tsx
@@ -1,17 +1,39 @@
+/* eslint-disable import/order */
 import '@testing-library/jest-dom';
-// import React from 'react';
-// import { render } from 'react-dom';
-// import MainPage from './Main';
-// import RegistrationPage from '../Registration/Registration';
-// import LoginPage from '../Login/Login';
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import MockLink from '@/__mocks__/MockLink';
+import swiperMocks from '@/__mocks__/MockSwiper';
+import { BrowserRouter } from 'react-router-dom';
+import Layout from '@/components/Layout/Layout';
 
-test('renders main page with header', () => {
-  // render(
-  //   <React.StrictMode>
-  //     <LoginPage />
-  //     <RegistrationPage />
-  //     <MainPage />
-  //   </React.StrictMode>,
-  //   document.getElementById('root')
-  // );
+jest.mock('swiper/react', () => swiperMocks);
+
+jest.mock('swiper', () => ({
+  default: jest.fn(),
+  Thumbs: jest.fn(),
+}));
+
+jest.mock('react-router-dom', () => MockLink);
+
+describe('Layout component', () => {
+  beforeEach(() => {
+    render(
+      <BrowserRouter>
+        <Layout />
+      </BrowserRouter>
+    );
+  });
+
+  test('render header', () => {
+    const loginButton = screen.getByText('Войти');
+    const registrationButton = screen.getByText('Регистрация');
+    expect(loginButton).toBeInTheDocument();
+    expect(registrationButton).toBeInTheDocument();
+  });
+
+  test('render footer', () => {
+    const socialNetworks = screen.getByText('Мы в социальных сетях');
+    expect(socialNetworks).toBeInTheDocument();
+  });
 });
