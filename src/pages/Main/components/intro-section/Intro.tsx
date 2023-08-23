9f9ac2e0771f905468bd44b16a69573941515c55
Dimkov Alexandr <dimkovaleks4@gmail.com>
2023-08-23 21:48:32 +0300

    feat: add page's links to intro section

--- src/pages/Main/components/intro-section/Intro.tsx
+++ src/pages/Main/components/intro-section/Intro.tsx
@@ -8,10 +8,60 @@ import svgShieldPath from '@/assets/images/svg/intro-card-shield.svg';
 import svgLikePath from '@/assets/images/svg/intro-card-like.svg';
 import svgHandsShieldPath from '@/assets/images/svg/intro-card-hands-shield.svg';
 import svgCasePath from '@/assets/images/svg/intro-card-case.svg';
+import Button from '@/components/Button/Button';
+import useAuth from '@/utils/hooks/useAuth';
 
 const Intro = (): ReactElement => {
+  const { signOut } = useAuth();
+
   return (
     <section className="_container intro">
+      <div className="intro__link-list-container">
+        <ul className="intro__links-list">
+          <li>
+            <Link className="header__link" to="/registration">
+              <Button
+                className={`header__button-main header__button-main--duplicate`}
+                type="button"
+                text="Регистрация"
+              />
+            </Link>
+          </li>
+          <li>
+            <Link className="header__link" to="/login">
+              <Button className={`header__button-main header__button-main--duplicate`} type="button" text="Войти" />
+            </Link>
+          </li>
+          <li>
+            <Link className="header__link" to="/">
+              <Button
+                onClick={(): void => signOut(() => 'f')}
+                className={`header__button-main header__button-main--duplicate`}
+                type="button"
+                text="Выйти"
+              />
+            </Link>
+          </li>
+        </ul>
+        <ul className="intro__links-list">
+          <li>
+            <Link className="header__link" to="/about-us">
+              <Button className={`header__button-main header__button-main--duplicate`} type="button" text="О нас" />
+            </Link>
+          </li>
+          <li>
+            <Link className="header__link" to="/catalog">
+              <Button className={`header__button-main header__button-main--duplicate`} type="button" text="Каталог" />
+            </Link>
+          </li>
+          <li>
+            <Link className="header__link" to="/cart">
+              <Button className={`header__button-main header__button-main--duplicate`} type="button" text="Корзина" />
+            </Link>
+          </li>
+        </ul>
+      </div>
+
       <Link to="/" className="intro__banner">
         <div className="intro__text-container">
           <p className="intro__promo-text">Акция действует до 31 декабря 2023</p>
