591e50e820c12e7f01475f3410840840456ca655
EvgeniiyaR <tulpansik@mail.ru>
2023-08-25 18:36:03 +0300

    feat: add user info in user profile page

--- src/pages/UserProfile/UserProfile.tsx
+++ src/pages/UserProfile/UserProfile.tsx
@@ -1,12 +1,29 @@
 import '@/pages/NotFound/NotFound.scss';
 import React, { ReactElement } from 'react';
+import useAuth from '@/utils/hooks/useAuth';
+import LayoutPreloader from '@/utils/helpers/Loader/LayoutPreloader/LayoutPreloader';
 
-function UserProfilePage(): ReactElement {
-  return (
+const UserProfilePage = (): ReactElement => {
+  const { userInfo, isContentLoadedPageUserInfo } = useAuth();
+
+  return isContentLoadedPageUserInfo ? (
+    <LayoutPreloader />
+  ) : (
     <div className="user-profile-page">
-      <h1 style={{ textAlign: 'center' }}>Личный кабинет (в разработке)</h1>;
+      <h1 style={{ textAlign: 'center' }}>Личный кабинет (в разработке)</h1>
+      {userInfo ? (
+        <>
+          <p>{userInfo.id}</p>
+          <p>{userInfo.email}</p>
+          <p>{userInfo.firstName}</p>
+          <p>{userInfo.lastName}</p>
+          <p>{userInfo.dateOfBirth}</p>
+        </>
+      ) : (
+        <p>Пользователь не авторизован</p>
+      )}
     </div>
   );
-}
+};
 
 export default UserProfilePage;
