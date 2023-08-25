31cde2556b4ed84e2a7eba42bee409d130822a62
EvgeniiyaR <tulpansik@mail.ru>
2023-08-25 18:20:34 +0300

    feat: add link to product

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -1,47 +1,50 @@
 import React, { ReactElement, useState } from 'react';
 import './Card.scss';
+import { Link } from 'react-router-dom';
 import { IPropsCard } from '@/types/componentsInrefaces';
 import Button from '../Button/Button';
 
-const Card = ({ imagePath, rating, text, price, priceBefore, bonus, discount }: IPropsCard): ReactElement => {
+const Card = ({ imagePath, rating, text, price, priceBefore, bonus, discount, id }: IPropsCard): ReactElement => {
   const [isSelected, setIsSelected] = useState(false);
 
   return (
-    <div className="product-card">
-      <div className="product-card__image-container">
-        <div className="product-card__discount">
-          <p className="product-card__discount-text">{`-${discount}%`}</p>
+    <Link to={`/catalog/${id}`}>
+      <div className="product-card">
+        <div className="product-card__image-container">
+          <div className="product-card__discount">
+            <p className="product-card__discount-text">{`-${discount}%`}</p>
+          </div>
+          <div className="product-card__image">
+            <img className="product-card__image-image" src={imagePath} alt="Product Image" />
+          </div>
         </div>
-        <div className="product-card__image">
-          <img className="product-card__image-image" src={imagePath} alt="Product Image" />
+        <div className="product-card__rating-container">
+          <div className="product-card__rating-icon"></div>
+          <p className="product-card__rating-text">{rating}</p>
+        </div>
+        <p className="product-card__text">{text}</p>
+        <div className="product-card__price-info">
+          <div className="product-card__price-container">
+            <p className="product-card__price">{`${price}₽`}</p>
+            <p className="product-card__priceBefore">{priceBefore}</p>
+          </div>
+          <div className="product-card__bonus-container">
+            <div className="product-card__bonus-icon"></div>
+            <p className="product-card__bonus-text">{`+${bonus}`}</p>
+          </div>
+        </div>
+        <div className="product-card__button-container">
+          <Button className="button" type="button" text="В корзину" onClick={(): void => {}} />
+          <Button
+            className={`iconButton ${isSelected ? 'iconButton__selected' : ''}`}
+            type="button"
+            onClick={(): void => {
+              setIsSelected(!isSelected);
+            }}
+          />
         </div>
       </div>
-      <div className="product-card__rating-container">
-        <div className="product-card__rating-icon"></div>
-        <p className="product-card__rating-text">{rating}</p>
-      </div>
-      <p className="product-card__text">{text}</p>
-      <div className="product-card__price-info">
-        <div className="product-card__price-container">
-          <p className="product-card__price">{`${price}₽`}</p>
-          <p className="product-card__priceBefore">{priceBefore}</p>
-        </div>
-        <div className="product-card__bonus-container">
-          <div className="product-card__bonus-icon"></div>
-          <p className="product-card__bonus-text">{`+${bonus}`}</p>
-        </div>
-      </div>
-      <div className="product-card__button-container">
-        <Button className="button" type="button" text="В корзину" onClick={(): void => {}} />
-        <Button
-          className={`iconButton ${isSelected ? 'iconButton__selected' : ''}`}
-          type="button"
-          onClick={(): void => {
-            setIsSelected(!isSelected);
-          }}
-        />
-      </div>
-    </div>
+    </Link>
   );
 };
 
