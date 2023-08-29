f22860ab5ff5cc1f0b06c1963b4d2175816a3af8
JuliaHoladava <juliaholadava@gmail.com>
2023-08-29 18:27:04 +0200

    feat: add product page

---
+++ src/pages/ProductPage/ProductPage.tsx
@@ -0,0 +1,25 @@
+import '@/pages/NotFound/NotFound.scss';
+import React, { ReactElement, useEffect } from 'react';
+import ProductCardsSection from '@/components/ProductCardsSection/ProductCardsSection';
+import DetailedProductSection from './components/DetailedProductSection';
+import ApiEndpoints from '@/enums/apiEndpoints';
+
+function ProductPage(): ReactElement {
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, []);
+
+  return (
+    <div className="product-page">
+      <DetailedProductSection />
+      <ProductCardsSection
+        header="Специальные предложения"
+        sectionClassName="special-offers"
+        counter={15}
+        url={ApiEndpoints.URL_PRODUCTS}
+      />
+    </div>
+  );
+}
+
+export default ProductPage;
