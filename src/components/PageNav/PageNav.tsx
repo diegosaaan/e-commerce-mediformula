9446d0b763d27b61592295a93009a5552a9ca68e
Dimkov Alexandr <dimkovaleks4@gmail.com>
2023-08-29 15:32:58 +0300

    feat: implement catalog search feature

--- src/components/PageNav/PageNav.tsx
+++ src/components/PageNav/PageNav.tsx
@@ -1,6 +1,6 @@
 /* eslint-disable no-nested-ternary */
 import './PageNav.scss';
-import React, { FormEvent, ReactElement } from 'react';
+import React, { FormEvent, ReactElement, KeyboardEvent, ChangeEvent, useState } from 'react';
 import { NavLink, Link, useNavigate } from 'react-router-dom';
 import { message } from 'antd';
 import useAuth from '@/utils/hooks/useAuth';
@@ -15,6 +15,8 @@ const setActive = ({ isActive }: { isActive: boolean }): string =>
 
 const PageNav = (): ReactElement => {
   const { isUserLoggedIn, isContentLoaded, signOut } = useAuth();
+  const [searchInputValue, setSearchInputValue] = useState('');
+
   const navigate = useNavigate();
 
   message.config({
@@ -36,12 +38,28 @@ const PageNav = (): ReactElement => {
     });
   };
 
-  const handleExampleSumbit = (e: FormEvent<HTMLFormElement>): void => {
-    e.preventDefault();
+  const handleSearchButtonClicked = (): void => {
+    if (searchInputValue) {
+      navigate('/catalog');
+    }
   };
 
-  const handleExampleChange = (e: FormEvent<HTMLInputElement>): void => {
-    e.preventDefault();
+  const handleSearchInputKeyDown = (event: KeyboardEvent): void => {
+    if (event.key === 'Enter') {
+      const target = event.target as HTMLInputElement;
+      if (target.value) {
+        navigate('/catalog');
+      }
+    }
+  };
+
+  const handleExampleSumbit = (event: FormEvent<HTMLFormElement>): void => {
+    event.preventDefault();
+  };
+
+  const handleChangeSearchInputValue = (event: ChangeEvent): void => {
+    const target = event.target as HTMLInputElement;
+    setSearchInputValue(target.value || '');
   };
 
   return (
@@ -133,9 +151,11 @@ const PageNav = (): ReactElement => {
                   name="header-search"
                   type="search"
                   placeholder="Поиск оборудования"
-                  onChange={handleExampleChange}
+                  value={searchInputValue}
+                  onChange={handleChangeSearchInputValue}
+                  onKeyDown={(event): void => handleSearchInputKeyDown(event)}
                 />
-                <Button className="header__button-search" type="submit" />
+                <Button className="header__button-search" type="submit" onClick={handleSearchButtonClicked} />
               </Form>
             </li>
             <li className="header__item-cart">
