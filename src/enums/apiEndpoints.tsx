3eb54ecf1c219a7158e82b8aa2c278b869b55e93
Dimkov Alexandr <dimkovaleks4@gmail.com>
2023-08-29 15:38:46 +0300

    feat: update APi endpoints (enum)

--- src/enums/apiEndpoints.tsx
+++ src/enums/apiEndpoints.tsx
@@ -8,6 +8,9 @@ enum ApiEndpoints {
   URL_AUTH_CUSTOMERS_TOKEN = 'https://auth.europe-west1.gcp.commercetools.com/oauth/e-shop230731/customers/token',
   URL_AUTH_INTROSPECT = 'https://auth.europe-west1.gcp.commercetools.com/oauth/introspect',
   URL_AUTH_TOKEN_ADMIN = `https://auth.europe-west1.gcp.commercetools.com/oauth/token`,
+  URL_CATALOG_PRODUCTS = `https://api.europe-west1.gcp.commercetools.com/e-shop230731/product-projections`,
+  URL_CATALOG_CATEGORIES = `https://api.europe-west1.gcp.commercetools.com/e-shop230731/categories`,
+  URL_CATALOG_DISCOUNTS = `https://api.europe-west1.gcp.commercetools.com/e-shop230731/product-discounts`,
 }
 
 export default ApiEndpoints;
