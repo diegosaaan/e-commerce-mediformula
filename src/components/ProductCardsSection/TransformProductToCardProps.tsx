3ae300154b404ab99550fa1a8d4d80e3790bb88c
JuliaHoladava <juliaholadava@gmail.com>
2023-08-29 12:09:25 +0200

    feat: transform product to card props

---
+++ src/components/ProductCardsSection/TransformProductToCardProps.tsx
@@ -0,0 +1,27 @@
+import { IPropsCard, IPropsProduct } from '@/types/componentsInrefaces';
+
+const TransformProductToCardProps = (product: IPropsProduct): IPropsCard => {
+  return {
+    imagePath: product.masterVariant.images[0]?.url || 'default-image.png',
+    rating: 4.8,
+    text: product.name.ru,
+    price:
+      product.masterVariant.prices[0]?.discounted?.value.centAmount ?? 0
+        ? (product.masterVariant.prices[0]?.discounted?.value.centAmount ?? 0) / 100
+        : (product.masterVariant.prices[0]?.value.centAmount ?? 0) / 100 || 0,
+    priceBefore: (product.masterVariant.prices[0]?.value.centAmount ?? 0) / 100 || 0,
+    bonus: 150,
+    discount:
+      product.masterVariant.prices[0]?.discounted?.value.centAmount ?? 0
+        ? Math.ceil(
+            (1 -
+              (product.masterVariant.prices[0]?.discounted?.value.centAmount ?? 0) /
+                (product.masterVariant.prices[0]?.value.centAmount ?? 0)) *
+              100
+          )
+        : 0,
+    onClick: (): void => window.scrollTo({ top: 0, behavior: 'smooth' }),
+  };
+};
+
+export default TransformProductToCardProps;
