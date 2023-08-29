f30c693da88fd9fe24efcf1a1ddc5b522ed2f70e
JuliaHoladava <juliaholadava@gmail.com>
2023-08-29 12:11:51 +0200

    feat: update product card sections on main page

--- src/pages/Main/Main.tsx
+++ src/pages/Main/Main.tsx
@@ -6,17 +6,28 @@ import Brends from './components/brends-section/Brends';
 import MediaSection from './components/media-section/Media';
 import Advantages from './components/advantages-section/Advantages';
 import Services from './components/services-section/Services';
+import ApiEndpoints from '@/enums/apiEndpoints';
 
 const MainPage = (): ReactElement => {
   return (
     <div className="main-page">
       <Intro />
-      <ProductCardsSection header="Популярное" counter={15} sectionClassName="popular" />
+      <ProductCardsSection
+        header="Популярное"
+        counter={15}
+        sectionClassName="popular"
+        url={ApiEndpoints.URL_PRODUCTS}
+      />
       <CategoryCards />
       <Brends />
       <Advantages />
       <Services />
-      <ProductCardsSection header="Специальные предложения" sectionClassName="special-offers" counter={15} />
+      <ProductCardsSection
+        header="Специальные предложения"
+        sectionClassName="special-offers"
+        counter={15}
+        url={ApiEndpoints.URL_PRODUCTS}
+      />
       <MediaSection />
     </div>
   );
