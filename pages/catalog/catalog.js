a8c7bcd60c387dcf679fd96735ce16470a03c47a
Dimkov Alexandr <dimkovaleks4@gmail.com>
2023-08-31 19:39:32 +0300

    feat: upload sprint_3 build v1 - catalog

--- pages/catalog/catalog.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunke_commerce=self.webpackChunke_commerce||[]).push([[774],{7506:(e,c,t)=>{t.r(c),t.d(c,{default:()=>l});var a=t(7294);const l=()=>a.createElement("div",{className:"catalog-page"},a.createElement("h1",{style:{textAlign:"center"}},"Страница каталога (в разработке)"),";")}}]);
