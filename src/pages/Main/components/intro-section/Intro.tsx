0fe1e9bafd938eb8bd9feb2b25cdcad0f73dc7bd
Dimkov Alexandr <dimkovaleks4@gmail.com>
2023-09-02 04:16:50 +0300

    feat: add ID to intro massage product route-link

--- src/pages/Main/components/intro-section/Intro.tsx
+++ src/pages/Main/components/intro-section/Intro.tsx
@@ -8,64 +8,14 @@ import svgShieldPath from '@/assets/images/svg/intro-card-shield.svg';
 import svgLikePath from '@/assets/images/svg/intro-card-like.svg';
 import svgHandsShieldPath from '@/assets/images/svg/intro-card-hands-shield.svg';
 import svgCasePath from '@/assets/images/svg/intro-card-case.svg';
-import Button from '@/components/Button/Button';
-import useAuth from '@/utils/hooks/useAuth';
 
 const Intro = (): ReactElement => {
-  const { signOut } = useAuth();
-
   return (
     <section className="_container intro">
-      <div className="intro__link-list-container">
-        <ul className="intro__links-list">
-          <li>
-            <Link className="header__link" to="/registration">
-              <Button
-                className={`header__button-main header__button-main--duplicate`}
-                type="button"
-                text="Регистрация"
-              />
-            </Link>
-          </li>
-          <li>
-            <Link className="header__link" to="/login">
-              <Button className={`header__button-main header__button-main--duplicate`} type="button" text="Войти" />
-            </Link>
-          </li>
-          <li>
-            <Link className="header__link" to="/">
-              <Button
-                onClick={(): void => signOut(() => 'f')}
-                className={`header__button-main header__button-main--duplicate`}
-                type="button"
-                text="Выйти"
-              />
-            </Link>
-          </li>
-        </ul>
-        <ul className="intro__links-list">
-          <li>
-            <Link className="header__link" to="/about-us">
-              <Button className={`header__button-main header__button-main--duplicate`} type="button" text="О нас" />
-            </Link>
-          </li>
-          <li>
-            <Link className="header__link" to="/catalog">
-              <Button className={`header__button-main header__button-main--duplicate`} type="button" text="Каталог" />
-            </Link>
-          </li>
-          <li>
-            <Link className="header__link" to="/cart">
-              <Button className={`header__button-main header__button-main--duplicate`} type="button" text="Корзина" />
-            </Link>
-          </li>
-        </ul>
-      </div>
-
-      <Link to="/" className="intro__banner">
+      <Link to="/catalog/97c54540-22c2-49a3-9f7c-87b0603975bd" className="intro__banner">
         <div className="intro__text-container">
           <p className="intro__promo-text">Акция действует до 31 декабря 2023</p>
-          <p className="intro__banner-text">Скидка на массажные накидки MEDISANA до 15%</p>
+          <p className="intro__banner-text">Скидка на массажную накидку ArhMed до 10%</p>
         </div>
         <div className="intro__image-container">
           <img className="intro__image" src={`${imagePath}`} alt="Intro image" />
