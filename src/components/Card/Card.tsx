efc3381779494bee02a969e2f4beb527565f9e7e
EvgeniiyaR <tulpansik@mail.ru>
2023-09-03 18:44:03 +0300

    fix: change layout Card

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -1,50 +1,32 @@
-import React, { ReactElement, useState } from 'react';
+import React, { ReactElement } from 'react';
 import './Card.scss';
-import { Link } from 'react-router-dom';
 import { IPropsCard } from '@/types/componentsInrefaces';
 import Button from '../Button/Button';
 
-const Card = ({ imagePath, rating, text, price, priceBefore, bonus, discount, id }: IPropsCard): ReactElement => {
-  const [isSelected, setIsSelected] = useState(false);
-
+const Card = ({ imagePath, text, price, priceBefore, discount }: IPropsCard): ReactElement => {
   return (
-    <Link to={`/catalog/${id}`}>
-      <div className="product-card">
-        <div className="product-card__image-container">
-          <div className="product-card__discount">
-            <p className="product-card__discount-text">{`-${discount}%`}</p>
-          </div>
-          <div className="product-card__image">
-            <img className="product-card__image-image" src={imagePath} alt="Product Image" />
-          </div>
+    <div className="product-card">
+      <div className="product-card__image-container">
+        <div className="product-card__discount">
+          <p className="product-card__discount-text">{`-${discount}%`}</p>
         </div>
-        <div className="product-card__rating-container">
-          <div className="product-card__rating-icon"></div>
-          <p className="product-card__rating-text">{rating}</p>
-        </div>
-        <p className="product-card__text">{text}</p>
-        <div className="product-card__price-info">
-          <div className="product-card__price-container">
-            <p className="product-card__price">{`${
-              price !== undefined ? Math.round(price).toLocaleString('ru-RU') : 'Не доступно'
-            }₽`}</p>
-            (<p className="product-card__priceBefore">{priceBefore?.toLocaleString('ru-RU')}</p>)
-          </div>
-          <div className="product-card__bonus-container">
-            <div className="product-card__bonus-icon"></div>
-            <p className="product-card__bonus-text">{`+${bonus}`}</p>
-          </div>
-        </div>
-        <div className="product-card__button-container">
-          <Button className="button" type="button" text="В корзину" onClick={(): void => {}} />
-          <Button
-            className={`iconButton ${isSelected ? 'iconButton__selected' : ''}`}
-            type="button"
-            onClick={(): void => setIsSelected(!isSelected)}
-          />
+        <div className="product-card__image">
+          <img className="product-card__image-image" src={imagePath} alt="Product Image" />
         </div>
       </div>
-    </Link>
+      <p className="product-card__text">{text}</p>
+      <div className="product-card__price-info">
+        <div className="product-card__price-container">
+          <p className="product-card__price">{`${
+            price !== undefined ? Math.round(price).toLocaleString('ru-RU') : 'Не доступно'
+          }₽`}</p>
+          <p className="product-card__priceBefore">{`${priceBefore?.toLocaleString('ru-RU')}₽`}</p>
+        </div>
+      </div>
+      <div className="product-card__button-container">
+        <Button className="button" type="button" text="В корзину" onClick={(): void => {}} />
+      </div>
+    </div>
   );
 };
 
