200a131c9220136ba03e91a0e62ea41d86581c9c
Dimkov Alexandr <dimkovaleks4@gmail.com>
2023-09-06 18:38:25 +0300

    feat: add preloaders to login and registration pages

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -1,5 +1,5 @@
 import '@/pages/Login/Login.scss';
-import React, { ReactElement } from 'react';
+import React, { ReactElement, useState } from 'react';
 import { message } from 'antd';
 import { useNavigate } from 'react-router-dom';
 import { Formik } from 'formik';
@@ -7,92 +7,99 @@ import { createNewUserToken, saveUserToken } from '@/services/tokenHelpers';
 import { LoginSchema } from '@/utils/helpers/yup/validationSchemes';
 import useAuth from '@/utils/hooks/useAuth';
 import AuthInput from '@/components/AuthInput/AuthInput';
-import AuthForm from '@/components/AuthForm/AuthForm';
+import AuthFormSection from '@/components/AuthFormSection/AuthFormSection';
 import { LoginSchemaType } from '@/types/types';
 import * as userAuth from '@/services/userAuth';
 import handleErrors from '@/utils/helpers/errorHandlers/errorHandlers';
 import { IUserTokenData } from '@/types/apiInterfaces';
+import SpinnerPreloader from '@/components/Preloaders/SpinnerPreloader/SpinnerPreloader';
 
 const LoginPage = (): ReactElement => {
   const { signIn } = useAuth();
   const navigate = useNavigate();
+  const [isDataFetching, setIsDataFetching] = useState(false);
 
-  const handleLogin = (values: LoginSchemaType): void => {
+  const handleLogin = async (values: LoginSchemaType): Promise<void> => {
+    setIsDataFetching(true);
     const { email, password } = values;
-    userAuth
-      .login(email, password)
-      .then(() => {
-        createNewUserToken(email, password).then((result) => {
-          if (result !== null && typeof result === 'object') {
-            message.info({
-              content: 'Добро пожаловать!',
-            });
-            signIn(() => navigate('/'));
-            saveUserToken(result as IUserTokenData);
-          }
-        });
-      })
-      .catch((error) => {
-        const {
-          response: {
-            data: { statusCode, message: errorMessage },
-          },
-        } = error;
 
-        handleErrors(statusCode, errorMessage);
-      });
+    try {
+      await userAuth.login(email, password);
+      const userTokenData = await createNewUserToken(email, password);
+      if (userTokenData !== null && typeof userTokenData === 'object') {
+        message.info({
+          content: 'Добро пожаловать!',
+        });
+        signIn(() => navigate('/'));
+        saveUserToken(userTokenData as IUserTokenData);
+      }
+    } catch (e) {
+      setIsDataFetching(false);
+      const error = e as { response: { data: { statusCode: number; message: string } } };
+      const {
+        response: {
+          data: { statusCode, message: errorMessage },
+        },
+      } = error;
+
+      handleErrors(statusCode, errorMessage);
+    }
   };
 
   return (
-    <Formik
-      initialValues={{
-        email: '',
-        password: '',
-      }}
-      validationSchema={LoginSchema}
-      onSubmit={handleLogin}
-    >
-      {({ values, errors, touched, handleChange, isValid, dirty }): JSX.Element => (
-        <AuthForm
-          name="form-login"
-          text="Еще не зарегистрированы?"
-          textLink="Регистрация"
-          textButton="Войти"
-          path="/registration"
-          title="Вход"
-          disabled={!isValid || !dirty}
-        >
-          <ul className="auth__list auth__list_active">
-            <li>
-              <AuthInput
-                type="text"
-                placeholder="Email*"
-                name="email"
-                htmlFor="email"
-                isInputPassword={false}
-                onChange={handleChange}
-                value={values.email}
-                errors={errors.email}
-                touched={touched.email}
-              />
-            </li>
-            <li>
-              <AuthInput
-                type="password"
-                placeholder="Пароль*"
-                name="password"
-                htmlFor="password"
-                isInputPassword={true}
-                onChange={handleChange}
-                value={values.password}
-                errors={errors.password}
-                touched={touched.password}
-              />
-            </li>
-          </ul>
-        </AuthForm>
-      )}
-    </Formik>
+    <>
+      {isDataFetching && <SpinnerPreloader pageClassname="login" isDataFetching={isDataFetching} />}
+      <Formik
+        initialValues={{
+          email: '',
+          password: '',
+        }}
+        validationSchema={LoginSchema}
+        onSubmit={handleLogin}
+      >
+        {({ values, errors, touched, handleChange, isValid, dirty }): JSX.Element => (
+          <AuthFormSection
+            name="form-login"
+            text="Еще не зарегистрированы?"
+            textLink="Регистрация"
+            textButton="Войти"
+            path="/registration"
+            title="Вход"
+            disabled={!isValid || !dirty}
+            isDataFetching={isDataFetching}
+          >
+            <ul className="auth__list auth__list_active">
+              <li>
+                <AuthInput
+                  type="text"
+                  placeholder="Email*"
+                  name="email"
+                  htmlFor="email"
+                  isInputPassword={false}
+                  onChange={handleChange}
+                  value={values.email}
+                  errors={errors.email}
+                  touched={touched.email}
+                />
+              </li>
+              <li>
+                <AuthInput
+                  type="password"
+                  placeholder="Пароль*"
+                  name="password"
+                  htmlFor="password"
+                  isInputPassword={true}
+                  onChange={handleChange}
+                  value={values.password}
+                  errors={errors.password}
+                  touched={touched.password}
+                />
+              </li>
+            </ul>
+          </AuthFormSection>
+        )}
+      </Formik>
+    </>
   );
 };
 
--- src/pages/Registration/Registration.tsx
+++ src/pages/Registration/Registration.tsx
@@ -3,18 +3,19 @@ import { Formik } from 'formik';
 import React, { ReactElement, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { message } from 'antd';
+import useAuth from '@/utils/hooks/useAuth';
 import { AddressType, RegisterSchemaType } from '@/types/types';
 import { createNewUserToken, saveUserToken } from '@/services/tokenHelpers';
 import AuthInput from '@/components/AuthInput/AuthInput';
-import AuthForm from '@/components/AuthForm/AuthForm';
+import AuthFormSection from '@/components/AuthFormSection/AuthFormSection';
 import ListAddress from '@/pages/Registration/components/ListAddress/ListAddress';
 import AddressFields from '@/pages/Registration/components/AddressFields/AddressFields';
 import { RegisterSchema } from '@/utils/helpers/yup/validationSchemes';
-import useAuth from '@/utils/hooks/useAuth';
 import { register } from '@/services/userAuth';
 import { IUserTokenData } from '@/types/apiInterfaces';
 import handleErrors from '@/utils/helpers/errorHandlers/errorHandlers';
 import getMinDateForRegister from '@/utils/helpers/getMinDateForRegister';
+import SpinnerPreloader from '@/components/Preloaders/SpinnerPreloader/SpinnerPreloader';
 
 const RegistrationPage = (): ReactElement => {
   const { signIn } = useAuth();
@@ -23,6 +24,7 @@ const RegistrationPage = (): ReactElement => {
   const [isAddAddress, setIsAddAddress] = useState(false);
   const [isShippingAddress, setIsShippingAddress] = useState(false);
   const [isBillingAddress, setIsBillingAddress] = useState(false);
+  const [isDataFetching, setIsDataFetching] = useState(false);
 
   const [shippingState, setShippingState] = useState({
     countryValue: 'Польша (PL)',
@@ -48,6 +50,8 @@ const RegistrationPage = (): ReactElement => {
   const [billingAddresses, setBillingAddresses] = useState<AddressType[]>([]);
 
   const handleRegister = async (values: RegisterSchemaType): Promise<void> => {
+    setIsDataFetching(true);
+
     const uniqueArray = Array.from(
       new Set([...shippingAddresses, ...billingAddresses].map((item) => JSON.stringify(item)))
     ).map((i) => JSON.parse(i));
@@ -110,10 +114,11 @@ const RegistrationPage = (): ReactElement => {
         signIn(() => navigate('/'));
         saveUserToken(tokenData as IUserTokenData);
       }
-    } catch (error: unknown) {
-      const err = error as { response: { data: { statusCode: number; message: string } } };
-      if (err.response && err.response.data) {
-        const { statusCode, message: errorMessage } = err.response.data;
+    } catch (e: unknown) {
+      setIsDataFetching(false);
+      const error = e as { response: { data: { statusCode: number; message: string } } };
+      if (error.response && error.response.data) {
+        const { statusCode, message: errorMessage } = error.response.data;
         handleErrors(statusCode, errorMessage);
       }
     }
@@ -168,220 +173,226 @@ const RegistrationPage = (): ReactElement => {
   };
 
   return (
-    <Formik
-      initialValues={{
-        firstName: '',
-        lastName: '',
-        date: `${getMinDateForRegister()}`,
-        email: '',
-        password: '',
-        passwordRepeat: '',
-        shipping: '',
-        billing: '',
-      }}
-      validationSchema={RegisterSchema}
-      onSubmit={handleRegister}
-    >
-      {({ values, errors, touched, handleChange, dirty }): JSX.Element => (
-        <AuthForm
-          name="form-register"
-          text="Уже зарегистрированы?"
-          textLink="Войти"
-          textButton="Зарегистрироваться"
-          path="/login"
-          title="Регистрация"
-          isRegister={true}
-          disabled={
-            !dirty ||
-            !!(
-              errors.firstName ||
-              errors.lastName ||
-              errors.date ||
-              errors.email ||
-              errors.password ||
-              errors.passwordRepeat
-            )
-          }
-          isAddAddress={isAddAddress}
-          handlePrevRegister={handlePrevRegister}
-        >
-          <ul
-            className={`auth__list auth__list_type_main ${isAddAddress ? 'auth__list_inactive' : 'auth__list_active'}`}
+    <>
+      {isDataFetching && <SpinnerPreloader pageClassname="registration" isDataFetching={isDataFetching} />}
+      <Formik
+        initialValues={{
+          firstName: '',
+          lastName: '',
+          date: `${getMinDateForRegister()}`,
+          email: '',
+          password: '',
+          passwordRepeat: '',
+          shipping: '',
+          billing: '',
+        }}
+        validationSchema={RegisterSchema}
+        onSubmit={handleRegister}
+      >
+        {({ values, errors, touched, handleChange, dirty }): JSX.Element => (
+          <AuthFormSection
+            name="form-register"
+            text="Уже зарегистрированы?"
+            textLink="Войти"
+            textButton="Зарегистрироваться"
+            path="/login"
+            title="Регистрация"
+            isRegister={true}
+            isDataFetching={isDataFetching}
+            disabled={
+              !dirty ||
+              !!(
+                errors.firstName ||
+                errors.lastName ||
+                errors.date ||
+                errors.email ||
+                errors.password ||
+                errors.passwordRepeat
+              )
+            }
+            isAddAddress={isAddAddress}
+            handlePrevRegister={handlePrevRegister}
           >
-            <li>
-              <AuthInput
-                type="text"
-                placeholder="Имя*"
-                name="firstName"
-                htmlFor="firstName"
-                isInputPassword={false}
-                value={values.firstName}
-                errors={errors.firstName}
-                touched={touched.firstName}
-                onChange={handleChange}
-              />
-            </li>
-            <li>
-              <AuthInput
-                type="text"
-                placeholder="Фамилия*"
-                name="lastName"
-                htmlFor="lastName"
-                isInputPassword={false}
-                value={values.lastName}
-                errors={errors.lastName}
-                touched={touched.lastName}
-                onChange={handleChange}
-              />
-            </li>
-            <li>
-              <AuthInput
-                type="date"
-                placeholder="Дата рождения*"
-                name="date"
-                htmlFor="date"
-                isInputPassword={false}
-                onChange={handleChange}
-                value={values.date}
-                errors={errors.date}
-                touched={touched.date}
-              />
-            </li>
-            <li>
-              <AuthInput
-                type="text"
-                placeholder="Email*"
-                name="email"
-                htmlFor="email"
-                isInputPassword={false}
-                onChange={handleChange}
-                value={values.email}
-                errors={errors.email}
-                touched={touched.email}
-              />
-            </li>
-            <li>
-              <AuthInput
-                type="password"
-                placeholder="Пароль*"
-                name="password"
-                htmlFor="password"
-                isInputPassword={true}
-                onChange={handleChange}
-                value={values.password}
-                errors={errors.password}
-                touched={touched.password}
-              />
-            </li>
-            <li>
-              <AuthInput
-                type="password"
-                placeholder="Повторите пароль*"
-                name="passwordRepeat"
-                htmlFor="passwordRepeat"
-                isInputPassword={true}
-                onChange={handleChange}
-                value={values.passwordRepeat}
-                errors={errors.passwordRepeat}
-                touched={touched.passwordRepeat}
-              />
-            </li>
-          </ul>
+            <ul
+              className={`auth__list auth__list_type_main ${
+                isAddAddress ? 'auth__list_inactive' : 'auth__list_active'
+              }`}
+            >
+              <li>
+                <AuthInput
+                  type="text"
+                  placeholder="Имя*"
+                  name="firstName"
+                  htmlFor="firstName"
+                  isInputPassword={false}
+                  value={values.firstName}
+                  errors={errors.firstName}
+                  touched={touched.firstName}
+                  onChange={handleChange}
+                />
+              </li>
+              <li>
+                <AuthInput
+                  type="text"
+                  placeholder="Фамилия*"
+                  name="lastName"
+                  htmlFor="lastName"
+                  isInputPassword={false}
+                  value={values.lastName}
+                  errors={errors.lastName}
+                  touched={touched.lastName}
+                  onChange={handleChange}
+                />
+              </li>
+              <li>
+                <AuthInput
+                  type="date"
+                  placeholder="Дата рождения*"
+                  name="date"
+                  htmlFor="date"
+                  isInputPassword={false}
+                  onChange={handleChange}
+                  value={values.date}
+                  errors={errors.date}
+                  touched={touched.date}
+                />
+              </li>
+              <li>
+                <AuthInput
+                  type="text"
+                  placeholder="Email*"
+                  name="email"
+                  htmlFor="email"
+                  isInputPassword={false}
+                  onChange={handleChange}
+                  value={values.email}
+                  errors={errors.email}
+                  touched={touched.email}
+                />
+              </li>
+              <li>
+                <AuthInput
+                  type="password"
+                  placeholder="Пароль*"
+                  name="password"
+                  htmlFor="password"
+                  isInputPassword={true}
+                  onChange={handleChange}
+                  value={values.password}
+                  errors={errors.password}
+                  touched={touched.password}
+                />
+              </li>
+              <li>
+                <AuthInput
+                  type="password"
+                  placeholder="Повторите пароль*"
+                  name="passwordRepeat"
+                  htmlFor="passwordRepeat"
+                  isInputPassword={true}
+                  onChange={handleChange}
+                  value={values.passwordRepeat}
+                  errors={errors.passwordRepeat}
+                  touched={touched.passwordRepeat}
+                />
+              </li>
+            </ul>
 
-          {!isAddAddress && (
-            <>
-              <button className="auth__button auth__button_type_add" type="button" onClick={handleAddAddresses}>
-                Добавить адреса
-              </button>
-            </>
-          )}
+            {!isAddAddress && (
+              <>
+                <button className="auth__button auth__button_type_add" type="button" onClick={handleAddAddresses}>
+                  Добавить адреса
+                </button>
+              </>
+            )}
 
-          <ul className={`auth__list ${isAddAddress ? 'auth__list_active' : 'auth__list_inactive'}`}>
-            <li>
-              <h2 className="auth__subtitle">Добавьте адреса для доставок</h2>
-              {!isShippingAddress && (
+            <ul className={`auth__list ${isAddAddress ? 'auth__list_active' : 'auth__list_inactive'}`}>
+              <li>
+                <h2 className="auth__subtitle">Добавьте адреса для доставок</h2>
+                {!isShippingAddress && (
+                  <>
+                    <button className="auth__button-add" type="button" onClick={handleOpenAddressShipping}></button>
+                    <ListAddress
+                      name="shipping"
+                      onChange={handleChange}
+                      addresses={shippingAddresses}
+                      setAddresses={setShippingAddresses}
+                      setAddressesAnother={setBillingAddresses}
+                      isAddress={isBillingAddress}
+                    />
+                    <p className="auth__help">
+                      Чтобы выбрать адрес для доставки по умолчанию кликните на карточку с адресом
+                    </p>
+                  </>
+                )}
+              </li>
+
+              {isShippingAddress && (
                 <>
-                  <button className="auth__button-add" type="button" onClick={handleOpenAddressShipping}></button>
-                  <ListAddress
+                  <AddressFields
+                    onChange={handleChange}
                     name="shipping"
-                    onChange={handleChange}
-                    addresses={shippingAddresses}
-                    setAddresses={setShippingAddresses}
-                    setAddressesAnother={setBillingAddresses}
-                    isAddress={isBillingAddress}
+                    addressesState={shippingState}
+                    setAddressesState={setShippingState}
                   />
-                  <p className="auth__help">
-                    Чтобы выбрать адрес для доставки по умолчанию кликните на карточку с адресом
-                  </p>
+                  <button
+                    className="auth__button auth__button_type_add"
+                    type="button"
+                    disabled={
+                      !!(shippingState.isPostalCodeError || shippingState.isCityError || shippingState.isStreetError)
+                    }
+                    onClick={(): void => handleAddShippingAddress()}
+                  >
+                    Добавить
+                  </button>
                 </>
               )}
-            </li>
 
-            {isShippingAddress && (
-              <>
-                <AddressFields
-                  onChange={handleChange}
-                  name="shipping"
-                  addressesState={shippingState}
-                  setAddressesState={setShippingState}
-                />
-                <button
-                  className="auth__button auth__button_type_add"
-                  type="button"
-                  disabled={
-                    !!(shippingState.isPostalCodeError || shippingState.isCityError || shippingState.isStreetError)
-                  }
-                  onClick={(): void => handleAddShippingAddress()}
-                >
-                  Добавить
-                </button>
-              </>
-            )}
+              <li>
+                <h2 className="auth__subtitle">Добавьте адреса для выставления счетов</h2>
+                {!isBillingAddress && (
+                  <>
+                    <button className="auth__button-add" type="button" onClick={handleOpenAddressBilling}></button>
+                    <ListAddress
+                      name="billing"
+                      onChange={handleChange}
+                      addresses={billingAddresses}
+                      setAddresses={setBillingAddresses}
+                      setAddressesAnother={setShippingAddresses}
+                      isAddress={isShippingAddress}
+                    />
+                    <p className="auth__help">
+                      Чтобы выбрать адрес для выставления счета по умолчанию кликните на карточку с адресом
+                    </p>
+                  </>
+                )}
+              </li>
 
-            <li>
-              <h2 className="auth__subtitle">Добавьте адреса для выставления счетов</h2>
-              {!isBillingAddress && (
+              {isBillingAddress && (
                 <>
-                  <button className="auth__button-add" type="button" onClick={handleOpenAddressBilling}></button>
-                  <ListAddress
-                    name="billing"
+                  <AddressFields
                     onChange={handleChange}
-                    addresses={billingAddresses}
-                    setAddresses={setBillingAddresses}
-                    setAddressesAnother={setShippingAddresses}
-                    isAddress={isShippingAddress}
+                    name="billing"
+                    addressesState={billingState}
+                    setAddressesState={setBillingState}
                   />
-                  <p className="auth__help">
-                    Чтобы выбрать адрес для выставления счета по умолчанию кликните на карточку с адресом
-                  </p>
+                  <button
+                    className="auth__button auth__button_type_add"
+                    type="button"
+                    disabled={
+                      !!(billingState.isPostalCodeError || billingState.isCityError || billingState.isStreetError)
+                    }
+                    onClick={(): void => handleAddBillingAddress()}
+                  >
+                    Добавить
+                  </button>
                 </>
               )}
-            </li>
-
-            {isBillingAddress && (
-              <>
-                <AddressFields
-                  onChange={handleChange}
-                  name="billing"
-                  addressesState={billingState}
-                  setAddressesState={setBillingState}
-                />
-                <button
-                  className="auth__button auth__button_type_add"
-                  type="button"
-                  disabled={
-                    !!(billingState.isPostalCodeError || billingState.isCityError || billingState.isStreetError)
-                  }
-                  onClick={(): void => handleAddBillingAddress()}
-                >
-                  Добавить
-                </button>
-              </>
-            )}
-          </ul>
-        </AuthForm>
-      )}
-    </Formik>
+            </ul>
+          </AuthFormSection>
+        )}
+      </Formik>
+    </>
   );
 };
 
