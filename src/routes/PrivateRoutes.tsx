c73045a7a912dec1dc20d19a020dcc53bbbb3a19
Dimkov Alexandr <dimkovaleks4@gmail.com>
2023-09-06 18:57:42 +0300

    refactor: rename private route component

--- src/routes/PrivateRoutes.tsx
+++ src/routes/PrivateRoutes.tsx
@@ -2,7 +2,7 @@ import React, { ReactElement } from 'react';
 import { Navigate } from 'react-router-dom';
 import useAuth from '@/utils/hooks/useAuth';
 
-export const PrivateLoginAndRegistrationRoute = ({ children }: { children: ReactElement }): ReactElement => {
+export const PrivateRouteForLoginAndRegistration = ({ children }: { children: ReactElement }): ReactElement => {
   const { isUserLoggedIn } = useAuth();
 
   if (isUserLoggedIn) {
