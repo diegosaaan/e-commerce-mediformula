36cc2a0757af93f692ad6182b0f64ac117017246
Dimkov Alexandr <dimkovaleks4@gmail.com>
2023-09-06 18:47:02 +0300

    refactor: add product data destructuring to detailed product section

--- src/pages/ProductPage/components/DetailedProductSection.tsx
+++ src/pages/ProductPage/components/DetailedProductSection.tsx
@@ -3,41 +3,71 @@ import 'swiper/css';
 import 'swiper/css/navigation';
 import 'swiper/css/free-mode';
 
-import React, { ReactElement, useState } from 'react';
+import React, { ReactElement, MouseEvent, useState } from 'react';
 import { Swiper, SwiperSlide } from 'swiper/react';
-import Modal from 'react-modal';
 import { Navigation } from 'swiper';
-import { Link } from 'react-router-dom';
 import Button from '@/components/Button/Button';
-import { IPropsDetailedProduct } from '@/types/componentsInrefaces';
 import arrowRightPath from '@/assets/images/svg/arrow-ahead.svg';
 import arrowLeftPath from '@/assets/images/svg/arrow-back.svg';
-import BrandIcons from './BrandIcons';
+import brandIcons from './brandIcons';
+import { IProductData } from '@/types/apiInterfaces';
 
 const DetailedProductSection = ({
-  productDetails,
+  productData,
   isDataFetching,
 }: {
-  productDetails: IPropsDetailedProduct;
+  productData: IProductData;
   isDataFetching: boolean;
 }): ReactElement => {
   const [isModalOpen, setIsModalOpen] = useState(false);
   const [currentIndex, setCurrentIndex] = useState(0);
 
+  const {
+    name: { ru: productName },
+    description: { ru: productDescription },
+    masterVariant: {
+      attributes,
+      images: productImages,
+      prices: [
+        {
+          value: { centAmount: defaultPrice },
+          discounted,
+        },
+      ],
+    },
+  } = productData;
+
+  const brandAttribute = attributes.find((attribute) => attribute.name === 'brand');
+  let brand = '';
+
+  if (brandAttribute && typeof brandAttribute.value === 'object' && 'ru' in brandAttribute.value) {
+    brand = brandAttribute.value.ru;
+  }
+
+  const isInStock = attributes.filter((attribute) => attribute.name === 'in-stock')[0].value as boolean;
+
+  let discountPrice = 0;
+  if (discounted) {
+    discountPrice = discounted.value.centAmount;
+  }
+
   const openModal = (index: number): void => {
     setCurrentIndex(index);
     setIsModalOpen(true);
-    document.body.style.overflow = 'hidden';
   };
 
-  const closeModal = (): void => {
-    setIsModalOpen(false);
-    document.body.style.overflow = 'scroll';
+  const closeModal = (event: MouseEvent): void => {
+    if (event.target) {
+      const target = event.target as HTMLDivElement;
+      if (target.classList.contains('detailed-product__modal-container') || target.classList.contains('close-button')) {
+        setIsModalOpen(false);
+      }
+    }
   };
 
   return (
     <section className={`_container detailed-product ${isDataFetching ? 'detailed-product--opacity' : ''}`}>
-      {productDetails ? (
+      {productData ? (
         <>
           <div className="detailed-product__image-container">
             <div className="detailed-product__image-block">
@@ -59,24 +89,29 @@ const DetailedProductSection = ({
                   }}
                   style={{ zIndex: isModalOpen ? 0 : 1 }}
                 >
-                  {productDetails.images.map((imageDetailes, index) => (
-                    <SwiperSlide key={index}>
-                      <Link to="#" onClick={(): void => openModal(index)}>
-                        <img
-                          className="detailed-product__image-image"
-                          src={imageDetailes.url}
-                          alt={imageDetailes.label}
-                        />
-                      </Link>
-                    </SwiperSlide>
+                  {productImages.map(({ url }, index) => (
+                    <SwiperSlide
+                      onClick={(): void => openModal(index)}
+                      key={index}
+                      style={{
+                        backgroundImage: `url(${url})`,
+                        backgroundRepeat: 'no-repeat',
+                        backgroundPosition: 'center',
+                        backgroundSize: 'contain',
+                      }}
+                    ></SwiperSlide>
                   ))}
                 </Swiper>
 
-                {isModalOpen && (
-                  <Modal isOpen={isModalOpen} onRequestClose={closeModal}>
-                    <a href="#" className="overlay" onClick={closeModal}></a>
-                    <div className="modal-window">
-                      {productDetails.images.length > 1 ? (
+                <div
+                  className={`detailed-product__modal-container ${
+                    isModalOpen ? 'detailed-product__modal-container--active' : ''
+                  }`}
+                  onClick={(event: MouseEvent): void => closeModal(event)}
+                >
+                  <div className="detailed-product__modal">
+                    {productImages.length > 1 ? (
+                      isModalOpen && (
                         <>
                           <button
                             className={`swiper-arrow detailed-product__swiper-arrow detailed-product__swiper-arrow--prev`}
@@ -87,6 +122,7 @@ const DetailedProductSection = ({
                               alt="Shevron Left"
                             />
                           </button>
+
                           <Swiper
                             initialSlide={currentIndex}
                             modules={[Navigation]}
@@ -97,14 +133,16 @@ const DetailedProductSection = ({
                               nextEl: `.detailed-product__swiper-arrow--next`,
                             }}
                           >
-                            {productDetails.images.map((imageDetailes, index) => (
-                              <SwiperSlide key={index}>
-                                <img
-                                  className="modal-window__image"
-                                  src={imageDetailes?.url}
-                                  alt={imageDetailes?.label}
-                                />
-                              </SwiperSlide>
+                            {productImages.map(({ url }, index) => (
+                              <SwiperSlide
+                                key={index}
+                                style={{
+                                  backgroundImage: `url(${url})`,
+                                  backgroundRepeat: 'no-repeat',
+                                  backgroundPosition: 'center',
+                                  backgroundSize: 'contain',
+                                }}
+                              ></SwiperSlide>
                             ))}
                           </Swiper>
                           <button
@@ -117,17 +155,13 @@ const DetailedProductSection = ({
                             />
                           </button>
                         </>
-                      ) : (
-                        <img
-                          className="modal-window__image"
-                          src={productDetails.images[0]?.url}
-                          alt={productDetails.images[0]?.label}
-                        />
-                      )}
-                      <div className="close-button" onClick={closeModal}></div>
-                    </div>
-                  </Modal>
-                )}
+                      )
+                    ) : (
+                      <img className="modal-window__image" src={productImages[0]?.url} alt={productImages[0]?.label} />
+                    )}
+                    <div className="close-button" onClick={(event: MouseEvent): void => closeModal(event)}></div>
+                  </div>
+                </div>
                 <button className={`swiper-arrow detailed-product__swiper-arrow detailed-product__swiper-arrow--next`}>
                   <img
                     className={`swiper-arrow-img detailed-product__swiper-arrow-img`}
@@ -139,28 +173,36 @@ const DetailedProductSection = ({
             </div>
           </div>
           <div className="detailed-product__description-block">
-            <h4 className="detailed-product__header">{productDetails.name}</h4>
-            <p className="detailed-product__description">{productDetails.description}</p>
+            <div
+              className={`${
+                isInStock ? 'detailed-product__product-is-stock' : 'detailed-product__product-no-is-stock'
+              }`}
+            >
+              {isInStock ? 'В наличии' : 'Нет в наличии'}
+            </div>
+            <h4 className="detailed-product__header">{productName}</h4>
+            <p className="detailed-product__description">{productDescription}</p>
             <div className="detailed-product__price-container">
-              <p className="detailed-product__price">{`${
-                productDetails.price !== undefined
-                  ? Math.round(productDetails.price).toLocaleString('ru-RU')
-                  : 'Не доступно'
-              }₽`}</p>
+              <p className="detailed-product__price">{`${(discountPrice
+                ? Math.round(discountPrice / 100)
+                : Math.round(defaultPrice / 100)
+              ).toLocaleString('ru-RU')}₽`}</p>
               <p className="detailed-product__priceBefore">{`${
-                productDetails.priceBefore !== undefined
-                  ? Math.round(productDetails.priceBefore).toLocaleString('ru-RU')
-                  : 'Не доступно'
-              }₽`}</p>
+                discountPrice ? `${Math.round(discountPrice / 100).toLocaleString('ru-RU')}₽` : ''
+              }`}</p>
             </div>
             <div className="detailed-product__button-container">
-              <Button className="button" type="button" text="В корзину" onClick={(): void => {}} />
+              <Button
+                className="button"
+                type="button"
+                text="В корзину"
+                onClick={(): void => {}}
+                disabled={!isInStock}
+              />
             </div>
           </div>
           <div className="detailed-product__added-block">
-            <div className="detailed-product__brand-image-container">
-              {productDetails.brand ? BrandIcons[productDetails.brand] || productDetails.brand : 'Загрузка...'}
-            </div>
+            <div className="detailed-product__brand-image-container">{brandIcons[brand]}</div>
             <div className="detailed-product__documentation-block">
               <h5 className="detailed-product__documentation-header">В комплекте</h5>
               <ul className="detailed-product__documentation-list">
